50e85c5a07d8db4fe59b29f72dc83d1f8fcc77e7
nethunder <17089113140@163.com>
2017-03-06 18:07:44 +0800

    fix

--- src/modules/practice/components/SubmitBox.tsx
+++ src/modules/practice/components/SubmitBox.tsx
@@ -15,7 +15,7 @@ export default class Discuss extends React.Component <any, any> {
 
   render() {
     return (
-      <div className="discuss-page">
+      <div className="discuss-page" style={{height:`${window.innerHeight}px`}}>
         <div className="submit">
           <textarea className="submit-area" cols="30" rows="10" height="500px" width="100%"
                     value={this.state.comment}
