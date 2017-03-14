5f4e6053fd27b60854ad64a50fcbe41a3b444c34
nethunder <17089113140@163.com>
2017-03-14 19:36:32 +0800

    fix

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -8,7 +8,8 @@ export default class Tutorial extends React.Component<any,any> {
     this.state = {
       bgList: ["http://www.iqycamp.com/images/riseTutorial0.png", "http://www.iqycamp.com/images/riseTutorial1.png",
         "http://www.iqycamp.com/images/riseTutorial2.png", "http://www.iqycamp.com/images/riseTutorial3.png",
-        "http://www.iqycamp.com/images/riseTutorial4.png", "http://www.iqycamp.com/images/riseTutorial5.png"],
+        "http://www.iqycamp.com/images/riseTutorial4.png", "http://www.iqycamp.com/images/riseTutorial5.5.png",
+        "http://www.iqycamp.com/images/riseTutorial5.png"],
       index: 0,
       onShowEnd: props.onShowEnd || function () {
         console.log('显示完成')
