01076fde3c3fb121a3e652a96a9809f050e6711e
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-03-26 10:32:31 +0800

    example analysis

--- src/modules/practice/warmup/ExampleAnalysis.tsx
+++ src/modules/practice/warmup/ExampleAnalysis.tsx
@@ -100,7 +100,7 @@ export class ExampleAnalysis extends React.Component <any, any> {
     const {knowledge} = this.state
     const {location} = this.props
     this.context.router.push({ pathname: '/rise/static/practice/warmup',
-      query: {kid: knowledge.id, practicePlanId: location.query.practicePlanId}})
+      query: {kid: knowledge.id, practicePlanId: location.query.practicePlanId, series:location.query.series}})
   }
 
   render() {
