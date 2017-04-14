4779306e6e9c39080b3d302a73e968bdfeb2d51c
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-04-14 11:02:19 +0800

    welcome page

--- src/modules/problem/Welcome.tsx
+++ src/modules/problem/Welcome.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { remove } from "lodash";
 import { welcome,trial,becomRiser } from "./async";
 import "./Welcome.less";
 import { welcome } from "./async";
 import { startLoad, endLoad, alertMsg } from "redux/actions";
+import Description from "./components/Description"
 
 @connect(state => state)
 export class Welcome extends React.Component <any, any> {
@@ -13,6 +13,9 @@ export class Welcome extends React.Component <any, any> {
     super();
     this.state = {
       show:false,
+      show2:false,
+      confirm:false,
+      description:false,
     }
   }
 
@@ -27,7 +30,11 @@ export class Welcome extends React.Component <any, any> {
     welcome().then((res)=>{
       dispatch(endLoad());
       if(res.code === 200){
-        this.setState({riseMember:res.msg});
+        if(res.msg){
+          this.context.router.push({
+            pathname: '/rise/static/problem/priority'
+          })
+        }
       }
     }).catch(ex=>{
       dispatch(endLoad());
@@ -59,33 +66,66 @@ export class Welcome extends React.Component <any, any> {
     window.location.href = `http://${window.location.hostname}/pay/pay`
   }
 
+  got(){
+    setTimeout(() => {
+      this.setState({show2:true, confirm:true})
+    }, 300)
+  }
+
+  closeModal(){
+    this.setState({description:false})
+  }
+
   render() {
-    const {show,riseMember} = this.state
+    const {show,show2,confirm,description} = this.state
 
-
-    const renderButton = (buttons)=>{
-      return buttons;
-    }
 
     return (
       <div>
-        <div className="problem-list">
-          <div className="info">
-            <img className={show?"show first":"hide first"} src="http://www.iqycamp.com/images/fragment/rise_welcome_1_1.png"/>
-            <img className={show?"show second":"hide second"} src="http://www.iqycamp.com/images/fragment/rise_welcome_2.png"/>
-            <img className={show?"show third":"hide third"} src="http://www.iqycamp.com/images/fragment/rise_welcome_3.png"/>
-          </div>
-          <div className="tips" onClick={()=>this.context.router.push("/rise/static/member/explain")}>
-            什么是专业版／试用版？
-          </div>
-        </div>
-        <div className="button-footer white-button">
-          {riseMember?<div onClick={()=>this.onSubmit()}>下一步</div>:
-            renderButton([<div className="button" onClick={()=>this.goTrial()}>试用版</div>,
-                        <div className="button" onClick={()=>this.becomeRiser()}>专业版</div>])
-          }
 
-        </div>
+        {description?
+            <Description closeModal={this.closeModal.bind(this)}/>:
+
+            <div className="problem-list">
+              <div className="info">
+                <div>
+                  <img className={show?"show first":"hide first"} src="http://www.iqycamp.com/images/fragment/rise_welcome_1_2.png"/>
+                </div>
+                <div>
+                  <img className={show?"show second":"hide second"} src="http://www.iqycamp.com/images/fragment/rise_welcome_2_2.png"/>
+                </div>
+                {!confirm?
+                    <div className="button-div">
+                      <img className={show?"show button1":"hide button1"} src="http://www.iqycamp.com/images/fragment/rise_welcome_confirm.png"
+                           onClick={this.got.bind(this)}/></div>:null
+                }
+                <div className="right-div">
+                  <img className={show2?"show third":"hide third"} src="http://www.iqycamp.com/images/fragment/rise_welcome_3_2.png"/>
+                  <img className={show2?"show head":"hide head"} src={window.ENV.headImage}/>
+                </div>
+                <div>
+                  <img className={show2?"show fourth":"hide fourth"} src="http://www.iqycamp.com/images/fragment/rise_welcome_4_2.png"/>
+                </div>
+                <div className="button-div">
+                  <img className={`${show2?"show button2":"hide button2"}`} src="http://www.iqycamp.com/images/fragment/rise_welcome_pay.png"
+                       onClick={()=>this.becomeRiser()}/>
+                </div>
+                <div className="button-div">
+                  <img className={`${show2?"show button3":"hide button3"}`} src="http://www.iqycamp.com/images/fragment/rise_welcome_try.png"
+                       onClick={()=>this.goTrial()}/>
+                </div>
+              </div>
+              <div className="little-bg">
+                <img width={110} height={113} src="http://www.iqycamp.com/images/fragment/rise_welcome_little_bg.png"/>
+              </div>
+              {show2?
+                  <div className="tips">
+                    <span>什么是专业版／试用版</span><img height={12} width={12} src="http://www.iqycamp.com/images/fragment/question_rise.png"
+                                                onClick={()=>this.setState({description:true})}/>
+                  </div>:null
+              }
+            </div>
+        }
       </div>
     )
   }
---
+++ src/modules/problem/components/Description.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import "./Description.less";
+import {startLoad, endLoad, alertMsg} from "../../../redux/actions";
+
+export default class Description extends React.Component <any, any> {
+
+    static contextTypes = {
+        router: React.PropTypes.object.isRequired
+    }
+
+
+    render() {
+        const {closeModal} = this.props
+        return (
+            <div className="description-page">
+                <div className="container has-footer">
+                    <div className="context">
+                        选择专业版，你可以学习RISE所有的专题，并收获：
+                        <ul>
+                            <li>系统学习所有知识</li>
+                            <li>将知识内化为能力</li>
+                            <li>解决实际工作问题</li>
+                            <li>参与案例分析直播</li>
+                            <li>得到圈外教练的反馈</li>
+                            <li>参加线下工作坊</li>
+                        </ul>
+                    </div>
+                    <div className="context">
+                        选择试用版，可以试学其中一个专题前3组的内容
+                    </div>
+                </div>
+                <div className="button-footer" onClick={()=>closeModal()}>返回</div>
+            </div>
+        )
+    }
+}
