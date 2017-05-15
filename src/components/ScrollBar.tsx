8d5f600257905208fae34a164fa18f06f50f6013
nethunder <17089113140@163.com>
2017-05-15 11:24:49 +0800

    update

---
+++ src/components/ScrollBar.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {merge} from 'lodash';
+
+export default class ScrollBar extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div></div>
+    )
+  }
+}
