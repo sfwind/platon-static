5df7d74e30fee5e4c80ea54ffff49192dc3a67c9
nethunder <17089113140@163.com>
2017-05-17 01:37:03 +0800

    update

--- src/modules/problem/Explore.tsx
+++ src/modules/problem/Explore.tsx
@@ -1,24 +1,82 @@
 import * as React from 'react';
+import { startLoad, endLoad, alertMsg } from "redux/actions";
 import { connect } from 'react-redux';
-import { ToolBar } from '../base/ToolBar'
-import Swiper from '../../components/Swiper'
+import { ToolBar } from '../base/ToolBar';
+import SwipeableViews from 'react-swipeable-views';
+import Banner from '../../components/Banner';
+import { loadUnChooseList } from './async';
 
+import './Explore.less';
 
+@connect(state=>state)
 export class Explore extends React.Component<any,any>{
   constructor(props){
     super(props);
     this.state = {
 
     }
+
+    this.problemWidth = window.innerWidth / 2.3;
+    this.problemHeight = 90/150 * this.problemWidth;
+  }
+
+  componentWillMount(){
+    const { dispatch } = this.props;
+    dispatch(startLoad());
+    loadUnChooseList().then(res => {
+      dispatch(endLoad());
+      if(res.code === 200){
+        console.log(res.msg);
+        this.setState({catalogList:res.msg.catalogList});
+      } else {
+        dispatch(alertMsg(res.msg));
+      }
+    }).catch(ex=>{
+      dispatch(endLoad());
+      dispatch(alertMsg(res.msg));
+    })
+
   }
 
   componentDidMount(){
+
   }
 
   render(){
+    const { catalogList } = this.state;
+
     return (
       <div className="explore-container">
-        <Swiper/>
+        <Banner height="200px">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </Banner>
+        <div className="problem-catalog-list">
+          {catalogList?catalogList.map((catalog,key)=>{
+            return (
+              <div className="problem-catalog">
+                <div className="header">
+                  <span className="catalog-name">{catalog.name}</span>
+                  <span className="catalog-more">更多</span>
+                </div>
+                <SwipeableViews containerStyle={{padding:'0 20px 0 0',width:`${this.problemWidth}px`}} slideStyle={{padding:'0 10px 0 0',width:`${this.problemWidth}px`}}>
+                  {catalog.problemList?catalog.problemList.map((problem,key)=>{
+                    return (
+                      <div className="problem-item">
+                        <img src={problem.pic}/>
+                        <span>{problem.problem}</span>
+                      </div>
+                    )
+                  }):null}
+                </SwipeableViews>
+              </div>
+            )
+          }):null}
+
+        </div>
+
         <ToolBar/>
       </div>
     )
