f17c27e5d4417c18d91d38556301dd2981a02a94
nethunder <17089113140@163.com>
2017-05-20 13:00:14 +0800

    修改inobounce

--- src/modules/practice/warmup/Analysis.tsx
+++ src/modules/practice/warmup/Analysis.tsx
@@ -47,6 +47,29 @@ export class Analysis extends React.Component <any, any> {
     if (this.props.location.query.practicePlanId !== newProps.location.query.practicePlanId) {
       this.componentWillMount(newProps)
     }
+    if(!this.props.iNoBounce && newProps.iNoBounce){
+      if(!newProps.iNoBounce.isEnabled()){
+        newProps.iNoBounce.enable();
+        alert('enable:'+newProps.iNoBounce.isEnabled());
+      }
+    }
+  }
+
+  componentDidMount(){
+    if(this.props.iNoBounce){
+      if(!this.props.iNoBounce.isEnabled()){
+        this.props.iNoBounce.enable();
+        alert('enable:'+this.props.iNoBounce.isEnabled());
+      }
+    }
+  }
+
+  componentWillUnmount(){
+    const {dispatch} = this.props;
+    if(this.props.iNoBounce){
+      this.props.iNoBounce.disable();
+      alert('diable:'+this.props.iNoBounce.isEnabled());
+    }
   }
 
   componentWillMount(props) {
@@ -65,6 +88,10 @@ export class Analysis extends React.Component <any, any> {
       dispatch(alertMsg(ex))
     })
   }
+
+
+
+
 
   next() {
     const {dispatch} = this.props
