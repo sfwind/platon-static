008dc96ba4a7b459d64efecd7b00908b317f99a5
nethunder <17089113140@163.com>
2017-05-22 10:26:59 +0800

    分数设置

--- src/modules/problem/components/ProblemItem.tsx
+++ src/modules/problem/components/ProblemItem.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './ProblemItem.less';
 import { merge } from 'lodash';
+const numeral = require('numeral');
 import AssetImg from "../../../components/AssetImg";
 
 export default class ProblemItem extends React.Component<any,any>{
@@ -39,7 +40,7 @@ export default class ProblemItem extends React.Component<any,any>{
              style={{width:`${(this.props.width?this.props.width:window.innerWidth) - this.picWidth - 35}px`}}>{problem.name}</p>
           <p className={`catalog sub-font ${this.picHeight < 67?'no-mg':''}`}>分类：{renderCatalog()}</p>
           <p className={`author  sub-font ${this.picHeight < 67?'no-mg':''}`}>讲师：{problem.author}</p>
-          <p className={`score  sub-font ${this.picHeight < 67?'no-mg':''}`}>难度系数：{problem.difficulty}/5.0</p>
+          <p className={`score  sub-font ${this.picHeight < 67?'no-mg':''}`}>难度系数：{numeral(problem.difficulty).format('0.0')}/5.0</p>
         </div>
       </div>: null
     )
