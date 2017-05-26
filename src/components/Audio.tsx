3d77acf7d94f3bf34b7d42832aba7d8c70d0e88b
三十文 <xfduan@duanxianfengdeMacBook-Pro.local>
2017-05-26 16:04:07 +0800

    stationary audio for ios

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -4,6 +4,7 @@ import Slider from "react-rangeslider";
 import AssetImg from "./AssetImg";
 
 let timer;
+let duration_load_timer;
 
 export default class Audio extends React.Component<any, any> {
   constructor(props) {
@@ -11,37 +12,64 @@ export default class Audio extends React.Component<any, any> {
     this.state = {
       duration: 0,
       currentSecond: 0,
+      cntSecond: 0,
       playing: false,
     }
   }
 
   onReady(e) {
-    // alert('canplay')
-    this.setState({ duration: e.target.duration })
+    this.setState({duration: e.target.duration})
   }
 
   onEnd() {
-    this.setState({ currentSecond: this.state.duration, playing: false })
+    this.setState({currentSecond: this.state.duration, playing: false})
     clearInterval(timer)
   }
 
   start() {
-    const self = this
-    if (timer) {
-      clearInterval(timer)
+    let self = this
+    // 首次点击播放按钮
+    if(this.state.duration === 0) {
+      duration_load_timer = setInterval(() => {
+        if(self.state.duration) {
+          clearInterval(duration_load_timer)
+          return
+        }
+        self.refs.sound.play()
+        self.refs.sound.pause()
+        if(self.refs.sound.duration) {
+          self.setState({duration: Math.ceil(self.refs.sound.duration)})
+          self.play()
+        }
+      }, 500)
     }
-    if (this.state.currentSecond >= this.state.duration) {
-      this.setState({ currentSecond: 0 })
+    // 暂停后重新播放
+    if(!this.state.playing && this.state.duration !== 0) {
+      this.play()
     }
-    this.setState({ playing: true })
-    timer = setInterval(() => {
-      self.setState({ currentSecond: self.state.currentSecond + 1 })
-    }, 1000)
-    this.refs.sound.play()
   }
 
+  play() {
+    const self = this
+    if(timer) {
+      clearInterval(timer)
+    }
+    this.setState({playing: true}, () => {
+      self.refs.sound.play()
+      timer = setInterval(() => {
+        if(this.state.currentSecond < this.state.duration) {
+          self.setState({currentSecond: self.state.currentSecond + 1})
+        } else {
+          this.setState({playing: false})
+          clearInterval(timer)
+        }
+      }, 1000)
+    })
+  }
+
+
   pause() {
-    this.setState({ playing: false })
+    this.setState({playing: false})
     clearInterval(timer)
     this.refs.sound.pause()
   }
@@ -50,49 +78,53 @@ export default class Audio extends React.Component<any, any> {
     clearInterval(timer)
     this.refs.sound.pause()
     this.refs.sound.load()
-    this.setState({ playing: false, currentSecond: 0 })
+    this.setState({playing: false, currentSecond: 0})
   }
 
+  //手动更改时间
   onProgressChange(value) {
-    if (value === 100) {
-      this.pause()
-      this.setState({ currentSecond: this.state.duration })
+    if(value >= this.state.duration) {
+      this.setState({currentSecond: this.state.duration + 1}, () => {
+        this.pause()
+      })
       return
     }
-    this.setState({ playing: true, currentSecond: value / 100 * this.state.duration })
     clearInterval(timer)
-    this.start()
-    this.refs.sound.currentTime = value / 100 * this.state.duration
-    this.refs.sound.play()
+    this.setState({playing: true, currentSecond: value}, () => {
+      this.refs.sound.currentTime = value
+      this.play()
+    })
   }
 
   render() {
-    const { url } = this.props
-    const { currentSecond, playing, duration } = this.state
-    // if ((!duration || duration < 0) && this.refs.sound && this.refs.sound.duration) {
-    // 	this.setState({ duration: this.refs.sound.duration })
-    // }
-
-    return false ?
+    const {url} = this.props
+    const {currentSecond, playing, duration} = this.state
+    return (
       <div className="audio">
         <div className="audio-container">
           { playing ?
-          <div className="audio-btn" onClick={this.pause.bind(this)}><AssetImg type="sound_active" size={20}/></div> :
-          <div className="audio-btn" onClick={this.start.bind(this)}><AssetImg type="sound" size={20}/></div>}
-          <div className="audio-duration">{ intToTime(currentSecond) }/{intToTime(duration)}</div>
+            <div className="audio-btn" onClick={this.pause.bind(this)}>
+              <AssetImg url="https://www.iqycamp.com/images/audio_pause.png" size={20}/>
+            </div> :
+            <div className="audio-btn" onClick={this.start.bind(this)}>
+              <AssetImg url="https://www.iqycamp.com/images/audio_play.png" size={20}/>
+            </div>
+          }
           <div className="audio-progress">
-            <Slider
-              value={currentSecond / duration * 100}
-              orientation="horizontal"
-              onChange={this.onProgressChange.bind(this)}
-            />
+            <Slider min={0} max={duration - 1} value={currentSecond} onChange={this.onProgressChange.bind(this)}
+                    withBars={true}/>
           </div>
-        </div>
+          <div className="audio-duration">
+            {intToTime(currentSecond)} / {intToTime(duration)}
+          </div>
           <audio ref="sound" src={url}
-               preload="auto"
-               onCanPlay={this.onReady.bind(this)}
-               onEnded={this.onEnd.bind(this)}/>
-      </div>:<audio src={url} ref="sound" controls="controls"></audio>
+                 preload="auto"
+                 onCanPlay={this.onReady.bind(this)}
+                 onEnded={this.onEnd.bind(this)}
+          />
+        </div>
+      </div>
+    )
   }
 }
 
