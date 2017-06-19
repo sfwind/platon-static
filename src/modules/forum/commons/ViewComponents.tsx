e2f59a55c2da7b964f45a097aa7c2adbc37b1f64
xfduan <1047210087@qq.com>
2017-06-19 20:26:14 +0800

    home page

--- src/modules/forum/commons/ViewComponents.tsx
+++ src/modules/forum/commons/ViewComponents.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import "./ViewComponents.less"
+import AssetImg from "../../../components/AssetImg";
 
+// 当网页最上层导航
 interface HeadAreaProps {
   content: string;
   btnContent?: string;
@@ -34,6 +36,8 @@ export class HeadArea extends React.Component<HeadAreaProps, any> {
 
 }
 
+
+// 问题概要 上侧组件
 interface QuestionHeadProps {
   leftImgUrl: string;
   quesUser: string;
@@ -43,5 +47,45 @@ interface QuestionHeadProps {
   rightContentFunc?: object;
 }
 export class QuestionHead extends React.Component<QuestionHeadProps, any> {
+
+  constructor() {
+    super()
+  }
+
+  render() {
+    const {
+      leftImgUrl, quesUser, quesTime, rightImgUrl, rightContent, rightContentFunc = () => {
+      }
+    } = this.props
+
+    const renderQuestionLeft = () => {
+      return (
+        <div className="question-left">
+          <div><AssetImg url={leftImgUrl} width={30} height={20}/></div>
+          <div>{quesUser}</div>
+          <div>{quesTime}</div>
+        </div>
+      )
+    }
+
+    const renderQuestionRight = () => {
+      const renderRight = rightImgUrl || rightContent || rightContentFunc;
+      if(renderRight) {
+        return (
+          <div className="question-right">
+            <div><AssetImg url={rightImgUrl} width={30} height={20}/></div>
+            <div onClick={() => rightContentFunc()} className="question-right-content">{rightContent}</div>
+          </div>
+        )
+      }
+    }
+
+    return (
+      <div className="question-head">
+        {renderQuestionLeft()}
+        {renderQuestionRight()}
+      </div>
+    )
+  }
 
 }
--- src/modules/forum/question/Question.tsx
+++ src/modules/forum/question/Question.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { HeadArea } from "../commons/ViewComponents";
+import { HeadArea, QuestionHead } from "../commons/ViewComponents";
 
 import "./Question.less";
 import AssetImg from "../../../components/AssetImg";
@@ -20,8 +20,16 @@ export default class Question extends React.Component<any, any> {
       return (
         <div className="ques-list">
           <div className="ques-desc">
-            <div >
-
+            <QuestionHead
+              leftImgUrl={``} quesUser={`三十文`} quesTime={`2012-01-01`}
+              rightImgUrl={``} rightContent={`关注问题`} rightContentFunc={() => console.log(`right content`)}
+            />
+            <div className="ques-title">跨行业跳槽应该注意什么？</div>
+            <div className="ques-content">
+              问题描述问题描述问题描述问题描述问题描述问题描述问题描述...
+            </div>
+            <div className="ques-answer-persons" onClick={() => console.log(`question persons`)}>
+              查看Virgo、孙圈圈等5人的回答
             </div>
           </div>
         </div>
@@ -30,7 +38,6 @@ export default class Question extends React.Component<any, any> {
 
     return (
       <div className="question-container">
-        <HeadArea content="你好" btnContent="下一步" btnFunc={() => console.log('question')}/>
         <div className="question-page">
           <div className="ques-nav">
             <div className="ques-nav-img"><AssetImg url="" height={30} width={40}/></div>
