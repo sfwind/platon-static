ec97055a8ef343b1ce377c38a67e180fd3278ad4
nethunder <17089113140@163.com>
2017-07-19 16:54:15 +0800

    Merge branches 'DEV_TEST_BRANCH' and 'dev_free_limit' of https://github.com/sfwind/platon-static into DEV_TEST_BRANCH
    # Conflicts:
    #	src/modules/problem/async.ts

--- src/modules/problem/Welcome.tsx
+++ src/modules/problem/Welcome.tsx
@@ -38,7 +38,7 @@ export default class Welcome extends React.Component<any, any> {
   handleClickGoTrailPage() {
     mark({ module: "打点", function: "付费相关", action: "点击试用版", memo: "欢迎页" }).then(() => {
       // TODO 配置限免小课跳转，后期删除
-      this.context.router.push('/rise/static/problem/view?id=9')
+      this.context.router.push('/rise/static/plan/view?id=9')
     })
   }
 
