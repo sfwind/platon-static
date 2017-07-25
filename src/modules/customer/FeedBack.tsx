af4ed7e017433888d5fcf71749df2ba389b12e1a
nethunder <17089113140@163.com>
2017-07-25 16:18:39 +0800

    temp update

--- src/modules/customer/FeedBack.tsx
+++ src/modules/customer/FeedBack.tsx
@@ -32,12 +32,6 @@ export default class FeedBack extends React.Component<any,any>{
             </div>
             <p style={{fontSize:'15px',fontWeight:'bolder'}}>常见问题</p><br/>
 
-            <p className="q">-如何进入小课的上／下一节？</p>
-            <p>-在屏幕下部「练习区」左右滑动即可</p><br/>
-
-            <p className="q">-我学完这节了，为什么还是启动不了下一节呢？</p>
-            <p>-每一节学完之后就直接能够进入下一节啦。但是需要保证上一节的「知识理解」和「巩固练习」的图标都标上了“√”哦（PS：「知识理解」务必点击“标记完成”哦）</p><br/>
-
             <p className="q">-可以在电脑端完成圈外小课练习吗？</p>
             <p>-可以登录www.iquanwai.com/community完成</p><br/>
 
@@ -48,11 +42,11 @@ export default class FeedBack extends React.Component<any,any>{
             <p className="q">-课程的老师是谁啊？</p>
             <p>-圈外的小课多数是圈圈设计、整个团队一同打磨出来的，另外的是和一些业界大V合作开发的</p><br/>
 
-            <p className="q">-之前报名的是专业版，现在我想升级成精英版，可以吗？</p>
-            <p>-可以的，补差价即可。差价=（升级时专业版或精英版的价格）－（您购买的价格）<br/>请微信添加圈外助手小Q完成付款（ID：quanwaizhushou）</p>
+            <p className="q">-之前报名的是半年版，现在我想升级成一年版，可以吗？</p>
+            <p>-可以的，补差价即可。<br/>请微信添加圈外助手小黑完成付款（ID：quanwaizhushou2）</p>
             <br/>
             <p className="q">啊哦～还想了解更多关于【圈外同学】使用的问题？戳链接直达吧——</p>
-            <p><a style={{color:'#666'}} href="https://shimo.im/doc/lRPFODOw4CY7eHfv/">{'https://shimo.im/doc/lRPFODOw4CY7eHfv/'}</a></p>
+            <p><a style={{color:'#55cbcb',textDecoration:'underline'}} href="https://shimo.im/doc/lRPFODOw4CY7eHfv/">{'https://shimo.im/doc/lRPFODOw4CY7eHfv/'}</a></p>
             <br/>
           </div>
         </div>
