1b6770123199e44f6339d5b000639f94814a92fb
nethunder <17089113140@163.com>
2017-08-03 17:27:04 +0800

    temp update

--- src/modules/plan/PlanList.tsx
+++ src/modules/plan/PlanList.tsx
@@ -1,34 +1,37 @@
 import * as React from "react";
 import "./PlanList.less";
-import {connect} from "react-redux";
-import {loadPlanList, updateOpenNavigator} from "./async";
-import {startLoad, endLoad, alertMsg} from "redux/actions";
-import {loadProblem, createPlan, checkCreatePlan,mark} from "./async";
+import { connect } from "react-redux";
+import { loadPlanList, updateOpenNavigator } from "./async";
+import { startLoad, endLoad, alertMsg } from "redux/actions";
+import { loadProblem, createPlan, checkCreatePlan, mark } from "./async";
 import AssetImg from "../../components/AssetImg";
 import { changeTitle } from "../../utils/helpers"
-import {ToolBar} from "../base/ToolBar"
+import { ToolBar } from "../base/ToolBar"
+import Swiper from 'swiper';
 
 import Tutorial from "../../components/Tutorial"
-import {isBoolean,merge} from "lodash"
+import { isBoolean, merge } from "lodash"
 
 /**
  * rise_icon_hr 左侧较宽 TODO
  */
-@connect(state=>state)
+@connect(state => state)
 export default class PlanList extends React.Component<any,any> {
   constructor() {
     super();
     this.state = {
-      openNavigator:true,
+      openNavigator: true,
     };
+    this.picWidth = (window.innerWidth - 15 - 10 - 10) / 2.5;
+    this.picHeight = (80 / 130) * this.picWidth;
     this.runPicWidth = (window.innerWidth / (750 / 270)) > 135 ? 135 : (window.innerWidth / (750 / 270));
     this.runTextWidth = (window.innerWidth - 30 - this.runPicWidth - 5);
     this.completedRightTextWidth = 45;
     let gup = 0;
-    if(window.innerWidth > 350){
+    if(window.innerWidth > 350) {
       gup = 30;
     }
-    this.completedLeftTextWidth = window.innerWidth - 56 - 84 - 8 - gup - 15 - 45 ;
+    this.completedLeftTextWidth = window.innerWidth - 56 - 84 - 8 - gup - 15 - 45;
     changeTitle("圈外同学");
   }
 
@@ -44,16 +47,17 @@ export default class PlanList extends React.Component<any,any> {
       memo: window.ENV.osName
     });
 
-    const { dispatch,location } = this.props;
+    const { dispatch, location } = this.props;
     dispatch(startLoad());
-    const {runningPlanId,completedPlanId} = location.query;
+    const { runningPlanId, completedPlanId } = location.query;
     loadPlanList().then(res => {
+      console.log(res);
       dispatch(endLoad());
       if(res.code === 200) {
-        const { runningPlans = [],completedPlans = [], trialClosedPlans= [], openNavigator } = res.msg;
+        const { runningPlans = [], completedPlans = [], trialClosedPlans = [], openNavigator, recommendations = [] } = res.msg;
         let showEmptyPage = false;
-        if(!runningPlans || runningPlans.length === 0){
-           showEmptyPage = true;
+        if(!runningPlans || runningPlans.length === 0) {
+          showEmptyPage = true;
         }
         if((!runningPlans || runningPlans.length === 0) && (!completedPlans || completedPlans.length === 0) && (!trialClosedPlans || trialClosedPlans.length === 0)) {
           if(location.pathname !== '/rise/static/learn') {
@@ -64,40 +68,30 @@ export default class PlanList extends React.Component<any,any> {
             return;
           }
         }
-        this.setState({planList:res.msg,showEmptyPage:showEmptyPage},()=>{
-          // // 第一次选课动画效果,ios有问题
-          // if(runningPlanId){
-          //   let planDom = document.querySelector(`#problem-${runningPlanId}`);
-          //   if(planDom){
-          //     planDom.style.cssText = `transform:translateY(-${planDom.offsetTop + planDom.offsetHeight}px)`;
-          //     setTimeout(()=>{
-          //       planDom.style.cssText = "transform:translateY(0)";
-          //     },1);
-          //   }
-          // }
-          // if(completedPlanId){
-          //   let planDom = document.querySelector(`#problem-${completedPlanId}`);
-          //   if(planDom) {
-          //     let box = document.querySelector('.plan-list-page');
-          //     box.scrollTop = planDom.offsetTop;
-          //   }
-          // }
+        this.setState({ planList: res.msg, showEmptyPage: showEmptyPage, recommendations: recommendations }, () => {
+          var swiper = new Swiper("#problem-recommendation", {
+            scrollbar: "#problem-recommendation-bar",
+            scrollbarHide: true,
+            slidesPerView: 'auto',
+            centeredSlides: false,
+            spaceBetween: 0,
+            grabCursor: true
+          });
         });
-        this.setState({planList:res.msg,showEmptyPage:showEmptyPage, openNavigator});
       } else {
         dispatch(alertMsg(res.msg));
       }
-    }).catch(ex=>{
+    }).catch(ex => {
       dispatch(endLoad());
       dispatch(alertMsg(ex));
     });
 
   }
 
-  handleClickPlan(plan){
+  handleClickPlan(plan) {
     this.context.router.push({
-      pathname:'/rise/static/plan/study',
-      query:{planId:plan.planId}
+      pathname: '/rise/static/plan/study',
+      query: { planId: plan.planId }
     })
   }
 
@@ -107,12 +101,34 @@ export default class PlanList extends React.Component<any,any> {
     })
   }
 
+  handleClickRecommend(problem) {
+    mark({
+      module: "打点",
+      function: "学习",
+      action: "学习页面点击推荐小课",
+      memo: window.ENV.osName
+    });
+    this.context.router.push({ pathname: '/rise/static/plan/view', query: { id: problem.id } });
+  }
+
+  handleClickMoreProblem(){
+    mark({
+      module: "打点",
+      function: "学习",
+      action: "学习页面点击发现更多",
+      memo: window.ENV.osName
+    });
+    this.context.router.push({
+      pathname: '/rise/static/problem/explore'
+    })
+  }
+
   tutorialEnd() {
-    const {dispatch} = this.props
+    const { dispatch } = this.props
     updateOpenNavigator().then(res => {
-      const {code, msg} = res
-      if (code === 200) {
-        this.setState({openNavigator: true})
+      const { code, msg } = res
+      if(code === 200) {
+        this.setState({ openNavigator: true })
       } else {
         dispatch(alertMsg(msg))
       }
@@ -121,12 +137,12 @@ export default class PlanList extends React.Component<any,any> {
 
   render() {
     const { location } = this.props;
-    const { runningPlanId,completedPlanId,trialClosedId } = location.query;
-    const { planList={},showEmptyPage, openNavigator } = this.state;
-    const { completedPlans = [],runningPlans = [],trialClosedPlans = []} = planList;
+    const { runningPlanId, completedPlanId, trialClosedId } = location.query;
+    const { planList = {}, showEmptyPage, openNavigator, recommendations = [] } = this.state;
+    const { completedPlans = [], runningPlans = [], trialClosedPlans = [] } = planList;
 
     const renderCompletedPlans = () => {
-      if(completedPlans){
+      if(completedPlans) {
         return completedPlans.map((plan, key) => {
 
           return (
@@ -140,7 +156,7 @@ export default class PlanList extends React.Component<any,any> {
     };
 
     const renderDeadline = (deadline) => {
-      if(deadline <0 ){
+      if(deadline < 0) {
         return null;
       } else {
         return (
@@ -165,7 +181,7 @@ export default class PlanList extends React.Component<any,any> {
             <div className="p-r-header">
               <span className="p-r-h-title">进行中</span>
             </div>
-            { showEmptyPage?
+            { showEmptyPage ?
               <div className="plp-empty-container">
                 <div className="plp-empty-img">
                   <AssetImg url="https://static.iqycamp.com/images/plan_empty.png" width={55} height={56}/>
@@ -173,23 +189,26 @@ export default class PlanList extends React.Component<any,any> {
                 <div className="plp-empty-text">
                   <span>还没有学习中的课程哦</span>
                 </div>
-                <div className="plp-empty-button"><span onClick={this.handleClickProblemChoose.bind(this)}>去选课</span></div>
+                <div className="plp-empty-button"><span onClick={this.handleClickProblemChoose.bind(this)}>去选课</span>
+                </div>
               </div>
               :
-              runningPlans.map((item,key) => {
+              runningPlans.map((item, key) => {
                 const { problem } = item;
                 let style = {};
-                if(runningPlanId==item.planId && false){
+                if(runningPlanId == item.planId && false) {
                   style = {
-                    transform:`translateY(${-window.innerHeight}px)`
+                    transform: `translateY(${-window.innerHeight}px)`
                   }
                 } else {
                   style = {
-                    transform:'0'
+                    transform: '0'
                   }
                 }
                 return (
-                  <div id={`problem-${item.planId}`} style={style} className={`p-r-block ${key === 0 ?'first':''} ${key === runningPlans.length - 1 ?'last':''}`} key={key} onClick={()=>this.handleClickPlan(item)}>
+                  <div id={`problem-${item.planId}`} style={style}
+                       className={`p-r-block ${key === 0 ?'first':''} ${key === runningPlans.length - 1 ?'last':''}`}
+                       key={key} onClick={()=>this.handleClickPlan(item)}>
                     <div className="p-r-b-item">
                       <div className="p-r-b-i-pic" style={{width:`${this.runPicWidth}px`}}>
                         <img className="p-r-b-i-pic-img" src={`${item.pic}`}/>
@@ -205,13 +224,51 @@ export default class PlanList extends React.Component<any,any> {
                       </div>
                     </div>
                   </div>
-                  );
+                );
               })}
           </div>
 
+          { recommendations && recommendations.length !== 0 ?
+            <div className="problem-recommendation ">
+              <div className="recommendation-header">
+                <span className="header-title">推荐小课</span>
+              </div>
+              <div className="swiper-container" id="problem-recommendation">
+                <div className="swiper-wrapper">
+                  {recommendations ? recommendations.map((problem, key) => {
+                    return (
+                      <div onClick={()=>this.handleClickRecommend(problem)} style={{width:`${this.picWidth}px`}}
+                           className="problem-item-show swiper-slide">
+                        <div className="img" style={{width:`${this.picWidth}px`,height:`${this.picHeight}px`}}>
+                          { problem.newProblem ?
+                            <AssetImg url="https://static.iqycamp.com/images/fragment/problem_new_icon_03.png"
+                                      style={{zIndex: 1, left: 0, top: 0}} size={25}/> : null
+                          }
+                          { problem.trial ?
+                            <AssetImg url="https://static.iqycamp.com/images/fragment/problem_trial_icon_01.png"
+                                      style={{zIndex: 1, left: 6, top: 6}} width={20}/> : null
+                          }
+                          <AssetImg url={`${problem.pic}`} style={{width:'auto',height:'100%'}}/>
+                        </div>
+                        <span>{problem.problem}</span>
+                      </div>
+                    )
+                  }) : null}
+                  <div onClick={()=>this.handleClickMoreProblem()} className="swiper-slide problem-item-show  found-more" style={{height:`${this.picHeight}px`}}>
+                    <div className="tips-word">
+                      发现更多
+                    </div>
+                    <div className="icon-next">
+                      <AssetImg type="rise_icon_arrow_right" size="25px"/>
+                    </div>
+                  </div>
+                </div>
+                <div className="swiper-scrollbar" id="problem-recommendation-bar"></div>
+              </div>
+            </div>: null}
 
 
-          { trialClosedPlans && trialClosedPlans.length !== 0?
+          { trialClosedPlans && trialClosedPlans.length !== 0 ?
             <div className="plp-completed plp-block">
               <div className="p-c-header">
                 <span className="p-c-h-title">试用到期</span>
@@ -222,9 +279,10 @@ export default class PlanList extends React.Component<any,any> {
                   </div>
                 </div>
                 <div className="p-c-c-right" style={{width:`${window.innerWidth - 56}px`}}>
-                  { trialClosedPlans.map((plan,key)=>{
+                  { trialClosedPlans.map((plan, key) => {
                     return (
-                      <div id={`problem-${plan.planId}`} className={`p-c-c-r-block ${key === 0 ? 'first':''}`} onClick={()=>this.handleClickPlan(plan)} >
+                      <div id={`problem-${plan.planId}`} className={`p-c-c-r-block ${key === 0 ? 'first':''}`}
+                           onClick={()=>this.handleClickPlan(plan)}>
                         <div className={`p-c-c-r-b-icon`}>
                           {/*<div className={`gap ${key === 0 ? 'first':''}`} />*/}
                           {/*<div className={`tick  ${key === 0 ? 'first':''}`}/>*/}
@@ -258,24 +316,25 @@ export default class PlanList extends React.Component<any,any> {
                   }) }
                 </div>
               </div>
-            </div>:null}
+            </div>: null}
 
-            <div className="plp-completed plp-block">
-              <div className="p-c-header">
-                <span className="p-c-h-title">已完成</span>
-              </div>
-              { completedPlans && completedPlans.length !== 0?
+          <div className="plp-completed plp-block">
+            <div className="p-c-header">
+              <span className="p-c-h-title">已完成</span>
+            </div>
+            { completedPlans && completedPlans.length !== 0 ?
               <div className="p-c-container">
                 <div className="p-c-c-left">
                   <div className="color-generator">
                   </div>
                 </div>
                 <div className="p-c-c-right" style={{width:`${window.innerWidth - 56}px`}}>
-                  { completedPlans.map((plan,key)=>{
+                  { completedPlans.map((plan, key) => {
                     return (
-                      <div id={`problem-${plan.planId}`} className={`p-c-c-r-block ${key === 0 ? 'first':''}`} onClick={()=>this.handleClickPlan(plan)} >
+                      <div id={`problem-${plan.planId}`} className={`p-c-c-r-block ${key === 0 ? 'first':''}`}
+                           onClick={()=>this.handleClickPlan(plan)}>
                         <div className={`p-c-c-r-b-icon`}>
-                          <div className={`gap ${key === 0 ? 'first':''}`} />
+                          <div className={`gap ${key === 0 ? 'first':''}`}/>
                           <div className={`tick  ${key === 0 ? 'first':''}`}/>
                           <div className={`hr ${key === completedPlans.length-1 ? 'last':''}`}/>
                           <div className={`bottom gap ${key === completedPlans.length -1?'last':'' }`}/>
@@ -308,9 +367,9 @@ export default class PlanList extends React.Component<any,any> {
                 </div>
               </div>
               :   <div className="complete-plan-empty">
-                    <AssetImg url='https://static.iqycamp.com/images/complete_plan_empty.png?imageslim' />
-                  </div>}
-            </div>
+              <AssetImg url='https://static.iqycamp.com/images/complete_plan_empty.png?imageslim'/>
+            </div>}
+          </div>
           <div className="padding-footer"/>
         </div>
       </div>
@@ -319,12 +378,12 @@ export default class PlanList extends React.Component<any,any> {
 }
 
 interface CardListProps {
-  pic:String,
-  title:String,
-  completeSeries?:String,
-  dayToClose?:String,
-  closeTime?:String,
+  pic: String,
+  title: String,
+  completeSeries?: String,
+  dayToClose?: String,
+  closeTime?: String,
 }
-class CardList extends React.Component<CardListProps,any>{
+class CardList extends React.Component<CardListProps,any> {
 
 }
