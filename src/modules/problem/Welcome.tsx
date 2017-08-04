93d53f25acd4833c250481a84d8158b5b2219959
xfduan <1047210087@qq.com>
2017-08-04 15:08:00 +0800

    fix welcome page

--- src/modules/problem/Welcome.tsx
+++ src/modules/problem/Welcome.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import './Welcome.less'
-import { mark, welcome } from "./async";
+import { mark, welcome } from './async'
 import { connect } from 'react-redux'
 import { startLoad, endLoad, alertMsg } from 'redux/actions'
+import AssetImg from '../../components/AssetImg'
 
 @connect(state => state)
 export default class Welcome extends React.Component<any, any> {
@@ -16,14 +17,14 @@ export default class Welcome extends React.Component<any, any> {
   }
 
   componentWillMount() {
-    mark({ module: "打点", function: "付费相关", action: "打开", memo: "欢迎页" });
+    mark({ module: '打点', function: '付费相关', action: '打开', memo: '欢迎页' })
     const { dispatch } = this.props
     dispatch(startLoad())
     welcome().then(res => {
       dispatch(endLoad())
       if(res.code === 200) {
         if(res.code === 200 && res.msg) {
-          this.context.router.push("/rise/static/problem/explore")
+          this.context.router.push('/rise/static/problem/explore')
         }
       } else {
         dispatch(alertMsg(res.msg))
@@ -34,13 +35,13 @@ export default class Welcome extends React.Component<any, any> {
   }
 
   handleClickGoTrailPage() {
-    mark({ module: "打点", function: "付费相关", action: "点击试用版", memo: "欢迎页" }).then(() => {
+    mark({ module: '打点', function: '付费相关', action: '点击试用版', memo: '欢迎页' }).then(() => {
       this.context.router.push('/rise/static/plan/view?id=9&free=true')
     })
   }
 
   handleClickGoExplorePage() {
-    mark({ module: "打点", function: "正式版本点击", action: "进入发现页面", memo: "欢迎页" }).then(() => {
+    mark({ module: '打点', function: '正式版本点击', action: '进入发现页面', memo: '欢迎页' }).then(() => {
       this.context.router.push(`/rise/static/problem/explore`)
     }).catch(() => {
       this.context.router.push(`/rise/static/problem/explore`)
@@ -48,7 +49,7 @@ export default class Welcome extends React.Component<any, any> {
   }
 
   handleClickGoPayPage() {
-    mark({ module: "打点", function: "付费相关", action: "点击成为RISER", memo: "欢迎页" }).then(() => {
+    mark({ module: '打点', function: '付费相关', action: '点击成为RISER', memo: '欢迎页' }).then(() => {
       window.location.href = `https://${window.location.hostname}/pay/pay`
     }).catch(() => {
       window.location.href = `https://${window.location.hostname}/pay/pay`
@@ -57,15 +58,19 @@ export default class Welcome extends React.Component<any, any> {
 
   render() {
     return (
-      <div className="welcome-container"
-           style={{ minHeight: (window.innerWidth / 375) * 667 - 50 }}>
+      <section className="welcome-container"
+               style={{ minHeight: (window.innerWidth / 375) * 667 - 50 }}>
         <div className="welcome-page">
-          <div className="welcome-botton">
-            <div className="button-left" onClick={() => this.handleClickGoTrailPage()}>限时免费</div>
-            <div className="button-right" onClick={() => this.handleClickGoExplorePage()}>所有小课</div>
+          <footer className="welcome-bottom">
+            <div className="button-left button" onClick={() => this.handleClickGoTrailPage()}>限时免费</div>
+            <div className="button-right button" onClick={() => this.handleClickGoExplorePage()}>所有小课</div>
+          </footer>
+          <div className="bottom-rise-tip">
+            <AssetImg url="https://static.iqycamp.com/images/fragment/problem_introduc_diamond_blue.jpg"/>
+            <span onClick={() => {window.location.href = '/pay/pay'}}>直接成为精英版年费会员</span>
           </div>
         </div>
-      </div>
+      </section>
     )
   }
 
