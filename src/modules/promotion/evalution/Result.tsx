e2552f64834cd63cb05ab6597d5a0c22ffe33e80
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-08-30 18:25:04 +0800

    change

--- src/modules/promotion/evalution/Result.tsx
+++ src/modules/promotion/evalution/Result.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { connect } from "react-redux";
-import { submitEva, shareResult } from "./async"
+import { submitEva } from "./async"
 import { startLoad, endLoad, alertMsg } from "../../../redux/actions";
 import { Dialog } from 'react-weui'
 import AssetImg from '../../../components/AssetImg'
@@ -19,7 +19,6 @@ export class Result extends React.Component<any,any> {
       backgroundPicHeight: 1334,
       percent: 0,
       learnFreeLimit: false,
-      picSrc: '',
     }
   }
 
@@ -71,37 +70,16 @@ export class Result extends React.Component<any,any> {
   }
 
   share() {
-    const { dispatch, location } = this.props
-    const { score } = location.query
-    const { learnFreeLimit, percent, picSrc } = this.state
-    this.setState({ share: true })
-
-    if(picSrc === '') {
-      dispatch(startLoad())
-      shareResult(score, percent).then(res => {
-        dispatch(endLoad())
-        if(res.code === 200) {
-          this.setState({ picSrc: res.msg })
-        } else {
-          dispatch(alertMsg(res.msg))
-        }
-      }).catch(e => {
-        dispatch(endLoad())
-        dispatch(alertMsg(e))
-      })
-    }
-
-  }
-
-  handleClickShareBtn() {
-    mark({ module: '打点', function: '测评', action: '点击页面领取海报按钮' });
-    this.setState({ showResult: true });
+    const { score } = this.props.location.query
+    const { percent, learnFreeLimit } = this.state
+    mark({ module: '打点', function: '测评', action: '点击页面领取海报按钮' })
+    this.context.router.push({pathname:'/rise/static/eva/share', query: {score, percent, learnFreeLimit}})
   }
 
   render() {
     const {
       initialScale, backgroundPicHeight, backgroundPicWidth, learnFreeLimit,
-      result, suggestion, percent, picSrc, share
+      result, suggestion, percent
     } = this.state
     return (
       <div className="eva-result">
@@ -122,33 +100,16 @@ export class Result extends React.Component<any,any> {
         { learnFreeLimit ? null :
           <div className="schedule">
             <AssetImg url="https://static.iqycamp.com/images/eva_schedule_3.png" width={'100%'}/>
+            <div className="class-more" onClick={()=>this.context.router.push('/rise/static/plan/view?id=9')}>了解更多</div>
           </div>}
         { learnFreeLimit ?
           <div className="free-get" onClick={()=>this.share()}>
-            <AssetImg url="https://static.iqycamp.com/images/free_get_5.png" height={57} width={226}/>
+            <AssetImg url="https://static.iqycamp.com/images/free_get_7.png" height={57} width={226}/>
           </div> :
           <div className="free-get" onClick={()=>this.share()}>
             <AssetImg url="https://static.iqycamp.com/images/free_get_6.png" height={57} width={226}/>
           </div>
         }
-
-        {share ?
-          <div className="dialog">
-            <div style={{ display: 'inline-block', float: 'right'}}
-                 onClick={()=>this.setState({share:false})}>
-              <AssetImg type="white_close_btn" size={24}/>
-            </div>
-            <div className="share-pic-container">
-              <div className="share-word-title">
-                【免费领取课程】
-              </div>
-              <pre className="share-word">
-                {learnFreeLimit ? '长按下方图片，发送给好友/群\n邀请他们也来挑战一下吧！'
-                  : `长按下方图片，发送给好友/群，邀请他们来挑战\n3人扫码完成测试，你就免费得到课程了！`}
-              </pre>
-              {picSrc ? <img className="share-pic" src={picSrc} width={'100%'}/> : null}
-            </div>
-          </div>: null}
       </div>
     )
   }
