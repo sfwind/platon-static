49d88bfd61a17273a96dc1a10f1c901514579b9c
xfduan <1047210087@qq.com>
2017-10-30 21:07:47 +0800

    draw code

--- src/modules/customer/account/components/HeadImage.tsx
+++ src/modules/customer/account/components/HeadImage.tsx
@@ -3,7 +3,7 @@ import './HeadImage.less'
 import { ppost } from '../../../../utils/request'
 import { connect } from 'react-redux'
 import { startLoad, endLoad, alertMsg } from 'redux/actions'
-import * as ExifJs from 'exif-js'
+import { adjustRotateImage } from '../../../../components/rotate/imageRotate'
 
 @connect(state => state)
 export class HeadImage extends React.Component {
@@ -36,103 +36,35 @@ export class HeadImage extends React.Component {
     showTab()
   }
 
-  readBlobAsDataURL(blob, callback) {
-    let reader = new FileReader()
-    reader.onload = function(e) {callback(e.target.result)}
-    reader.readAsDataURL(blob)
-  }
-
-  dataURLtoBlob(dataurl) {
-    let arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n)
-    while(n--) {
-      u8arr[n] = bstr.charCodeAt(n)
-    }
-    return new Blob([u8arr], { type: mime })
-  }
-
-  getExifJsFile(file) {
-    return new Promise((resolve) => {
-      ExifJs.getData(file, () => {
-        let orientation = ExifJs.getTag(file, 'Orientation')
-        resolve(orientation)
-      })
-    })
-  }
-
-  rotateImage(file, orientation) {
-    return new Promise((resolve) => {
-      this.readBlobAsDataURL(file, (dataUrl) => {
-        let image = new Image()
-        image.src = dataUrl
-        image.onload = () => {
-          let canvas = document.createElement('canvas')
-          canvas.width = image.width
-          canvas.height = image.height
-          let halfWidth = image.width / 2
-          let halfHeight = image.height / 2
-          let ctx = canvas.getContext('2d')
-          ctx.translate(halfWidth, halfHeight)
-          switch(orientation) {
-            case 1:
-              break
-            case 3: // 被翻转了180度
-              // 解决：图片再次翻转180度
-              ctx.rotate(180 * Math.PI / 180)
-              break
-            case 6: // 顺时针旋转了90度，
-              // 解决：需要将图片逆时针旋转90度
-              ctx.rotate(90 * Math.PI / 180)
-              break
-            case 8: // 逆时针旋转了90度
-              // 解决：需要将图片顺时针旋转90度
-              ctx.rotate(90 * Math.PI / 180)
-              break
-            default:
-              break
-          }
-          ctx.translate(-halfWidth, -halfHeight)
-          ctx.drawImage(image, 0, 0)
-
-          let rotateDataUrl = canvas.toDataURL('image/jpeg')
-          let rotateUrlBlob = this.dataURLtoBlob(rotateDataUrl)
-          resolve(rotateUrlBlob)
-        }
-      })
-    })
-  }
-
   autoUploadHeadImg() {
     let node = this.refs.changeImg
     this.timer = setInterval(() => {
       if(node && node.files && node.files[0]) {
         let formData = new FormData()
         let file = node.files[0]
-        this.getExifJsFile(file).then(orientation => {
-          this.rotateImage(file, orientation).then(rotateBlob => {
-            formData.append('file', rotateBlob)
-            node = undefined
-            this.refs.changeImg.value = null
-            this.dispatch(startLoad())
-            ppost(`/rise/customer/profile/headImg/upload`, formData).then(res => {
-              clearInterval(this.timer)
-              if(res.code === 200) {
-                this.setState({
-                  headImgUrl: res.msg,
-                  modify: false
-                }, () => {
-                  this.refs.headImg.onload = () => {
-                    this.dispatch(endLoad())
-                  }
-                })
-              } else {
-                this.dispatch(endLoad())
-                this.dispatch(alertMsg(res.msg))
-              }
-            }).catch(e => {
+        adjustRotateImage(file, rotateBlob => {
+          formData.append('file', rotateBlob)
+          node = undefined
+          this.refs.changeImg.value = null
+          this.dispatch(startLoad())
+          ppost(`/rise/customer/profile/headImg/upload`, formData).then(res => {
+            clearInterval(this.timer)
+            if(res.code === 200) {
+              this.setState({
+                headImgUrl: res.msg,
+                modify: false
+              }, () => {
+                this.refs.headImg.onload = () => {
+                  this.dispatch(endLoad())
+                }
+              })
+            } else {
               this.dispatch(endLoad())
-              this.dispatch(alertMsg(e))
-            })
+              this.dispatch(alertMsg(res.msg))
+            }
+          }).catch(e => {
+            this.dispatch(endLoad())
+            this.dispatch(alertMsg(e))
           })
         })
       }
