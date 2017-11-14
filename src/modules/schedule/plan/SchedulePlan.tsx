60e2013e6115ce9c0b065b3270a4d564077bcd41
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-11-15 07:41:47 +0800

    add url and open course

--- src/modules/schedule/plan/SchedulePlan.tsx
+++ src/modules/schedule/plan/SchedulePlan.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import './SchedulePlan.less'
 import { connect } from 'react-redux'
-import { loadSchedulePlan } from './async'
+import { loadSchedulePlan, createPlan } from './async'
 import { startLoad, endLoad, alertMsg } from 'redux/actions'
 import { changeTitle } from '../../../utils/helpers'
 import { mark } from '../../../utils/request'
@@ -9,6 +9,10 @@ import { Dialog, Progress } from 'react-weui'
 import AssetImg from '../../../components/AssetImg'
 
 const { Alert } = Dialog
+
+const MAJOR_PROBLEM = 1;
+const MINOR_PROBLEM = 2;
+const TRIAL_PROBLEM = 3;
 
 /**
  * rise_icon_hr 左侧较宽 TODO
@@ -52,6 +56,49 @@ export default class SchedulePlan extends React.Component<any, any> {
     })
   }
 
+  clickCourse(type, item){
+    const { dispatch } = this.props
+
+    if(type === TRIAL_PROBLEM){
+      this.context.router.push({pathname:'/rise/static/plan/study', query:{planId:item.id}})
+    }
+
+    // 没有开课
+    if(!item.id){
+      this.setState({
+        dialogButtons: [
+          {
+            label: '取消',
+            onClick: () => {
+              this.setState({ dialogShow: false, dialogButtons: [], dialogContent: '' })
+            }
+          },
+          {
+            label: '确认',
+            onClick: () => {
+              dispatch(startLoad())
+              this.setState({ dialogShow: false })
+              createPlan(item.problem.id).then(res => {
+                dispatch(endLoad())
+                if(res.code === 200) {
+                  this.context.router.push(`/rise/static/plan/study?planId=${res.msg}`)
+                } else {
+                  dispatch(alertMsg(res.msg))
+                }
+              }).catch(e => {
+                dispatch(alertMsg(e))
+              })
+            }
+          }
+        ],
+        dialogShow: true,
+        dialogContent: '小课开启后，学习期限为30天。期间完成学习即可永久查看内容。确认开启吗？'
+      })
+    }else{
+      this.context.router.push({pathname:'/rise/static/plan/study', query:{planId:item.id}})
+    }
+  }
+
   learn(item){
     this.context.router.push({pathname:'/rise/static/plan/study', query:{planId:item.id}})
   }
@@ -64,13 +111,19 @@ export default class SchedulePlan extends React.Component<any, any> {
     const renderMajorCourse = ()=>{
       return majorProblem.map((item, index)=>{
           return (
-            <div className="course-card">
+            <div className="course-card" onClick={()=>this.clickCourse(MAJOR_PROBLEM, item)} key={index}>
               <div className="img">
-                <div className={`problem-item-backcolor catalog4`}/>
-                <div className={`problem-item-backimg catalog4`}/>
+                <div className={`problem-item-backcolor major`}/>
+                <div className={`problem-item-backimg`}/>
                 <div className="problem-item-subCatalog">{item.problem.abbreviation}</div>
+                <div className="problem-month major">
+                  <span className="month-large">{item.month}</span>
+                  <span className="month-small">{'月'}</span>
+                </div>
+                {item.id ? null : <div className="wait-open">待开课</div> }
               </div>
               <div className="problem-name">{item.problem.problem}</div>
+
             </div>
           )
         }
@@ -80,11 +133,16 @@ export default class SchedulePlan extends React.Component<any, any> {
     const renderMinorCourse = ()=>{
       return minorProblem.map((item, index)=>{
         return (
-          <div className="course-card">
+          <div className="course-card" onClick={()=>this.clickCourse(MINOR_PROBLEM, item)} key={index}>
             <div className="img">
-              <div className={`problem-item-backcolor catalog1`}/>
-              <div className={`problem-item-backimg catalog1`}/>
+              <div className={`problem-item-backcolor minor`}/>
+              <div className={`problem-item-backimg`}/>
               <div className="problem-item-subCatalog">{item.problem.abbreviation}</div>
+              <div className="problem-month minor">
+                <span className="month-large">{item.month}</span>
+                <span className="month-small">{'月'}</span>
+              </div>
+              {item.id ? null : <div className="wait-open">待开课</div> }
             </div>
             <div className="problem-name">{item.problem.problem}</div>
           </div>
@@ -95,10 +153,10 @@ export default class SchedulePlan extends React.Component<any, any> {
     const renderTrialCourse = ()=>{
       return trialProblem.map((item, index)=>{
         return (
-          <div className="course-card">
+          <div className="course-card" key={index}>
             <div className="img">
-              <div className={`problem-item-backcolor catalog3`}/>
-              <div className={`problem-item-backimg catalog3`}/>
+              <div className={`problem-item-backcolor trial`}/>
+              <div className={`problem-item-backimg`}/>
               <div className="problem-item-subCatalog">{item.problem.abbreviation}</div>
             </div>
             <div className="problem-name">{item.problem.problem}</div>
@@ -112,7 +170,7 @@ export default class SchedulePlan extends React.Component<any, any> {
         var year = this.moment(item.closeTime).format('YYYY')
         var date = this.moment(item.closeTime).format('MM.DD')
         return (
-          <div className="complete-plan">
+          <div className="complete-plan" key={index}>
             <div className="plan-close">
               <div className="plan-close-date">{date}</div>
               <div className="plan-close-year">{year}</div>
@@ -134,6 +192,19 @@ export default class SchedulePlan extends React.Component<any, any> {
       )
     }
 
+    const renderDialog = () => {
+      const { dialogShow = false, dialogButtons = [], dialogContent } = this.state
+
+      return (
+        <Alert
+          show={dialogShow}
+          buttons={dialogButtons}
+        >
+          {dialogContent}
+        </Alert>
+      )
+    }
+
     return (
       <div className="schedule-plan-container" style={{minHeight:window.innerHeight}}>
 
@@ -141,7 +212,7 @@ export default class SchedulePlan extends React.Component<any, any> {
           { topic ? month+'月 '+topic: null}
         </div>
         <div className="schedule-plan">
-          <div className="plan-button">
+          <div className="plan-button" onClick={()=>this.context.router.push('/rise/static/course/schedule/overview')}>
             {'学习计划'}
           </div>
         </div>
@@ -192,6 +263,7 @@ export default class SchedulePlan extends React.Component<any, any> {
             {renderCompleteCourse()}
           </div>
         </div>
+        {renderDialog()}
       </div>
     )
   }
--- src/modules/schedule/plan/async.ts
+++ src/modules/schedule/plan/async.ts
@@ -3,3 +3,7 @@ import { pget, ppost } from 'utils/request'
 export function loadSchedulePlan(){
   return pget('/rise/schedule//load/plan')
 }
+
+export function createPlan(problemId) {
+  return ppost(`/rise/plan/choose/problem/${problemId}`)
+}
