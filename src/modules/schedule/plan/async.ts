954470ffab0482dae51b020f6dcd86e76a16f3d0
nethunder <17089113140@163.com>
2017-11-15 17:55:43 +0800

    update

--- src/modules/schedule/plan/async.ts
+++ src/modules/schedule/plan/async.ts
@@ -1,7 +1,7 @@
 import { pget, ppost } from 'utils/request'
 
 export function loadSchedulePlan(){
-  return pget('/rise/schedule//load/plan')
+  return pget('/rise/schedule/load/plan')
 }
 
 export function createPlan(problemId) {
