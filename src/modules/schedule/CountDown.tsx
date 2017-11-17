c75b44b094113cf9a7105799de24601f04faebc3
nethunder <17089113140@163.com>
2017-11-17 19:50:29 +0800

    update

--- src/modules/schedule/CountDown.tsx
+++ src/modules/schedule/CountDown.tsx
@@ -5,6 +5,8 @@ import './CountDown.less';
 import { loadCountDownInfo } from './async'
 import AssetImg from '../../components/AssetImg'
 import { preview } from '../helpers/JsConfig'
+import { chooseAuditionCourse } from './async';
+import { mark } from '../../utils/request'
 
 let _ = require('lodash');
 
@@ -28,12 +30,12 @@ export default class CountDown extends Component {
     loadCountDownInfo().then(res => {
       dispatch(endLoad())
       if(res.code === 200) {
-        const { days, hasSchedule } = res.msg;
+        const { days, hasSchedule, hasAudition } = res.msg;
         if(days <= 0) {
           if(hasSchedule) {
-            this.context.router.push({pathname:'/rise/static/plan/main'});
+            this.context.router.push({ pathname: '/rise/static/plan/main' });
           } else {
-            this.context.router.push({pathname:'/rise/static/course/schedule/start'})
+            this.context.router.push({ pathname: '/rise/static/course/schedule/start' })
           }
           return;
         }
@@ -53,7 +55,7 @@ export default class CountDown extends Component {
             ones = daysStr[ 0 ];
           }
         }
-        this.setState({ days: days, ones: ones, tens: tens });
+        this.setState({ days: days, ones: ones, tens: tens, hasAudition: hasAudition });
       } else {
         dispatch(alertMsg(res.msg));
       }
@@ -62,8 +64,37 @@ export default class CountDown extends Component {
     })
   }
 
+  handleClickAudition() {
+    // 开试听课
+    const { dispatch } = this.props
+    dispatch(startLoad())
+    chooseAuditionCourse().then(res => {
+      dispatch(endLoad())
+      if(res.code === 200) {
+        const { planId, goSuccess, errMsg, startTime, endTime } = res.msg
+        if(errMsg) {
+          mark({ module: '打点', function: '试听课', action: '无法开启试听课', memo: '倒计时页面' })
+          dispatch(alertMsg(errMsg))
+        } else {
+          if(goSuccess) {
+            mark({ module: '打点', function: '试听课', action: '开通试听课', memo: '倒计时页面' })
+            window.location.href = `https://${window.location.hostname}/pay/static/audition/success`
+          } else {
+            mark({ module: '打点', function: '试听课', action: '进入试听课', memo: '倒计时页面' })
+            this.context.router.push('/rise/static/plan/main');
+          }
+        }
+      } else {
+        dispatch(alertMsg(res.msg))
+      }
+    }).catch(ex => {
+      dispatch(endLoad())
+      dispatch(alertMsg(ex))
+    })
+  }
+
   render() {
-    const { ones, tens } = this.state;
+    const { ones, tens, hasAudition } = this.state;
     return (
       <div className="count-down">
         <div className="header">
@@ -77,6 +108,9 @@ export default class CountDown extends Component {
             <div className="ones-place place">
               {ones}
             </div>
+            {
+              hasAudition ? <div className="auditions" onClick={() => this.handleClickAudition()}>试听课</div> : null
+            }
           </div>
         </div>
         <div className="footer">
