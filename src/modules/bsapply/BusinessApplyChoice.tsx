93b01c9047dbfbae20b5235e4ed7913754e586b2
nethunder <17089113140@163.com>
2017-11-27 09:51:42 +0800

    update

--- src/modules/bsapply/BusinessApplyChoice.tsx
+++ src/modules/bsapply/BusinessApplyChoice.tsx
@@ -156,7 +156,7 @@ export default class BusinessApplyChoice extends Component<any, any> {
     submitApply({ userSubmits: result }).then(res => {
       dispatch(endLoad());
       if(res.code === 200) {
-        // this.context.router.push('/rise/static/business/apply/submit/success');
+        this.context.router.push('/rise/static/business/apply/submit/success');
       } else {
         dispatch(alertMsg(res.msg));
       }
