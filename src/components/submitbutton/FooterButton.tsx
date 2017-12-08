6b2a514ea9afd01c40e72cb27079c5e3210acb5d
xfduan <1047210087@qq.com>
2017-12-08 13:53:35 +0800

    add style

--- src/components/submitbutton/FooterButton.tsx
+++ src/components/submitbutton/FooterButton.tsx
@@ -2,7 +2,10 @@ import * as React from 'react'
 import './FooterButton.less'
 
 interface SubmitButtonProps {
-  btnArray: any,
+  btnArray: {
+    click: any,
+    text: string
+  }
 }
 
 interface SubmitButtonState {
@@ -18,24 +21,25 @@ export class FooterButton extends React.Component<SubmitButtonProps, SubmitButto
   render() {
     const { btnArray = [] } = this.props
     if(btnArray.length === 1) {
-      const { click, text } = btnArray[ 0 ];
+      const { click, text } = btnArray[0]
       return (
         <div className="ft-button-wrapper button-footer">
           <div className="submit-btn" onClick={() => click()}>{text}</div>
         </div>
       )
     } else {
-      //if(btnArray.length === 2)
       return (
         <div className="ft-button-wrapper button-footer two-buttons">
-          {btnArray.map((btn, idx) => {
-            const { click, text } = btn;
-            return (
-              <div className="button" key={idx} onClick={() => click()}>
-                {text}
-              </div>
-            )
-          })}
+          {
+            btnArray.map((btn, idx) => {
+              const { click, text } = btn
+              return (
+                <div className="button" key={idx} onClick={() => click()}>
+                  {text}
+                </div>
+              )
+            })
+          }
         </div>
       )
     }
