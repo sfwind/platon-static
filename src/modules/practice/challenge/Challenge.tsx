5a01df9cbd4dd93da33b4a13138030295d1cbde8
xfduan <1047210087@qq.com>
2017-12-08 15:25:10 +0800

    fix editor render block bug

--- src/modules/practice/challenge/Challenge.tsx
+++ src/modules/practice/challenge/Challenge.tsx
@@ -10,6 +10,7 @@ import { mark } from '../../../utils/request'
 import AssetImg from '../../../components/AssetImg'
 import { SubmitButton } from '../../../components/submitbutton/SubmitButton'
 import { ColumnSpan } from '../../../components/ColumnSpan'
+import { Block } from '../../../components/Block'
 
 @connect(state => state)
 export class Challenge extends React.Component <any, any> {
@@ -21,8 +22,7 @@ export class Challenge extends React.Component <any, any> {
       submitId: 0,
       page: 1,
       otherList: [],
-      opacity: 0,
-      edit: true
+      opacity: 0
     }
     this.pullElement = null
   }
@@ -107,67 +107,49 @@ export class Challenge extends React.Component <any, any> {
   }
 
   render() {
-    const { data, edit, showDisable } = this.state
+    const { data, edit = false, showDisable } = this.state
     const { content } = data
 
-    const renderTip = () => {
-      if(edit) {
-        return (
-          <div className="no-comment">
-            <div className="content">
-              <div className="text">更喜欢电脑上提交?</div>
-              <div className="text">登录www.iquanwai.com/community</div>
-            </div>
-          </div>
-        )
-      } else {
-        return (
-          <div>
-            <Work onEdit={() => this.onEdit()} operation={false}
-                  headImage={window.ENV.headImage} userName={window.ENV.userName} {...data}/>
-          </div>
-        )
-      }
-    }
-
     return (
-      <div>
-        <div className="challenge-container">
-          <div className="challenge-header">小目标</div>
-          <div className="challenge-context">
-            你有什么目标，可以利用本课程的训练实现呢？制定目标帮你更积极地学习，也带给你更多成就感！
-          </div>
+      <div className="challenge-container">
+        <div className="challenge-header">小目标</div>
+        <div className="challenge-context">
+          你有什么目标，可以利用本课程的训练实现呢？制定目标帮你更积极地学习，也带给你更多成就感！
+        </div>
 
-          <div className="intro-container">
-            <AssetImg className='challenge-image'
-                      url="https://static.iqycamp.com/images/fragment/challenge_practice_2.png"/>
-            <div className="challenge-tips">
-              <p className="tip-title">小提示</p>
-              <p className="tip">本题答案仅自己可见</p>
-              <p className="tip">目标最好是某个具体问题或场景</p>
-              <p className="tip">制定目标之前，可以先回顾该课程的知识体系</p>
-            </div>
-          </div>
-
-          <ColumnSpan height={20}/>
-
-          <div ref="workContainer" className="work-container">
-            <div className="submit-bar">{content === null ? '提交方式' : '我的目标'}</div>
-            {renderTip()}
-            {
-              edit &&
-              <div className="editor">
-                <Editor ref="editor" moduleId={3} value={content}
-                        placeholder="有灵感时马上记录在这里吧，系统会自动为你保存。完成后点下方按钮提交，就会得到点赞和专业点评哦！"/>
-              </div>
-            }
+        <div className="intro-container">
+          <AssetImg className='challenge-image'
+                    url="https://static.iqycamp.com/images/fragment/challenge_practice_2.png"/>
+          <div className="challenge-tips">
+            <p className="tip-title">小提示</p>
+            <p className="tip">本题答案仅自己可见</p>
+            <p className="tip">目标最好是某个具体问题或场景</p>
+            <p className="tip">制定目标之前，可以先回顾该课程的知识体系</p>
           </div>
         </div>
+
+        <ColumnSpan/>
+
         {
-          showDisable ?
-            <SubmitButton clickFunc={() => {}} buttonText={'提交中'}/> :
-            edit &&
-            <SubmitButton clickFunc={() => this.onSubmit()} buttonText={'提交'}/>
+          edit ?
+            <Block>
+              <div className="working-tip">
+                <p>更喜欢电脑上提交?</p>
+                <p>登录www.iquanwai.com/community</p>
+              </div>
+              <Editor ref="editor" moduleId={3} value={content}
+                      placeholder="有灵感时马上记录在这里吧，系统会自动为你保存。完成后点下方按钮提交，就会得到点赞和专业点评哦！"/>
+              {
+                showDisable ?
+                  <SubmitButton clickFunc={() => {}} buttonText={'提交中'}/> :
+                  <SubmitButton clickFunc={() => this.onSubmit()} buttonText={'提交'}/>
+              }
+            </Block> :
+            <Block>
+              <div className="working-header">我的目标</div>
+              <Work onEdit={() => this.onEdit()} operation={false}
+                    headImage={window.ENV.headImage} userName={window.ENV.userName} {...data}/>
+            </Block>
         }
       </div>
     )
--- src/modules/practice/components/NewWork.tsx
+++ src/modules/practice/components/NewWork.tsx
@@ -1,136 +1,137 @@
-import * as React from "react";
-import "./NewWork.less"
-import {isString,truncate,isFunction} from "lodash";
-import AssetImg from "../../../components/AssetImg";
-import { preview } from "../../helpers/JsConfig"
-import { Dialog } from "react-weui"
+import * as React from 'react'
+import './NewWork.less'
+import { isString, truncate, isFunction } from 'lodash'
+import AssetImg from '../../../components/AssetImg'
+import { preview } from '../../helpers/JsConfig'
+import { Dialog } from 'react-weui'
+
 const { Alert } = Dialog
-import {connect} from "react-redux";
-import {requestCommentByType, increaseArticleShow} from "../../message/async"
-import {alertMsg} from "../../../redux/actions";
+import { connect } from 'react-redux'
+import { requestCommentByType, increaseArticleShow } from '../../message/async'
+import { alertMsg } from '../../../redux/actions'
 
 @connect(state => state)
-export default class Work extends React.Component<any,any> {
-
-
+export default class Work extends React.Component<any, any> {
 
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       showAll: false,
-      filterContent:isString(props.content)?this.simpleContent(props.content):"",
-      showRequestComment:false,
-      request:false,
+      filterContent: isString(props.content) ? this.simpleContent(props.content) : '',
+      showRequestComment: false,
+      request: false
     }
   }
 
-  simpleContent(content){
+  simpleContent(content) {
     //删除标签，删除空格，删除空行
-    return content.replace(/<[^>]+>/g,"").replace(/&nbsp;/g,"").replace(/^(\s*)\n/g, "")
+    return content.replace(/<[^>]+>/g, '').replace(/&nbsp;/g, '').replace(/^(\s*)\n/g, '')
   }
 
-  disOpen(filterContent,showAll){
-    const { wordsCount=60 } = this.props;
-    return filterContent.length>wordsCount && !showAll;
+  disOpen(filterContent, showAll) {
+    const { wordsCount = 60 } = this.props
+    return filterContent.length > wordsCount && !showAll
   }
 
-  requestComment(){
+  requestComment() {
     this.onRequestComment()
-    this.setState({showRequestComment:false})
+    this.setState({ showRequestComment: false })
   }
 
-  componentWillReceiveProps(nextProps){
-    if(nextProps.content && !this.props.content){
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.content && !this.props.content) {
       this.setState({
-        filterContent:isString(nextProps.content)?this.simpleContent(nextProps.content):""
+        filterContent: isString(nextProps.content) ? this.simpleContent(nextProps.content) : ''
       })
     }
   }
 
-  componentWillMount(){
-    this.setState({request:this.props.request})
+  componentWillMount() {
+    this.setState({ request: this.props.request })
   }
 
-  contentClick(e){
-    if(e.target.tagName === 'IMG'){
+  contentClick(e) {
+    if(e.target.tagName === 'IMG') {
       let item = e.target.src
-      let picList = Array.from(this.refs.submitContent.querySelectorAll('img')).map(item=>item.src);
+      let picList = Array.from(this.refs.submitContent.querySelectorAll('img')).map(item => item.src)
       preview(item, picList)
     }
   }
 
-  click(){
-    const { dispatch, requestCommentCount } = this.props;
-    const { request } = this.state;
-    if(request){
-      dispatch(alertMsg('本练习已经使用过求点评啦'));
-      return;
+  click() {
+    const { dispatch, requestCommentCount } = this.props
+    const { request } = this.state
+    if(request) {
+      dispatch(alertMsg('本练习已经使用过求点评啦'))
+      return
     }
-    if(requestCommentCount===0){
-      dispatch(alertMsg('本课程求点评次数已用完'));
-      return;
+    if(requestCommentCount === 0) {
+      dispatch(alertMsg('本课程求点评次数已用完'))
+      return
     }
-    this.setState({showRequestComment:true})
+    this.setState({ showRequestComment: true })
   }
 
   onRequestComment() {
-    const { dispatch, submitId, type } = this.props;
-    requestCommentByType(type, submitId).then(res =>{
-      let {code,msg} = res;
-      if(code===200){
-        this.setState({request:true})
+    const { dispatch, submitId, type } = this.props
+    requestCommentByType(type, submitId).then(res => {
+      let { code, msg } = res
+      if(code === 200) {
+        this.setState({ request: true })
         dispatch(alertMsg('教练已经收到你的请求啦\n点评后，会在消息中心通知你的'))
       } else {
-        dispatch(alertMsg(msg));
+        dispatch(alertMsg(msg))
       }
     })
 
   }
 
-  show(showAll){
-    const { articleModule ,submitId} = this.props;
-    if(!showAll && articleModule){
+  show(showAll) {
+    const { articleModule, submitId } = this.props
+    if(!showAll && articleModule) {
       // 增加浏览量
-      increaseArticleShow(articleModule,submitId);
+      increaseArticleShow(articleModule, submitId)
     }
-    this.setState({showAll:!showAll})
+    this.setState({ showAll: !showAll })
   }
 
   render() {
-    const {headImage, userName, content,
-      submitUpdateTime,onEdit,voteCount,commentCount,
-      voteStatus,onVoted,goComment,wordsCount=60,
-      title,avatarStyle = 'left', role, signature, requestCommentCount,feedback=false,
-      operation=true} = this.props;
-    const {showAll,filterContent, showRequestComment,request} = this.state;
-    const renderWorkContent = ()=>{
-      if(isString(content)){
-        if(filterContent.length>wordsCount && !showAll){
+    const {
+      headImage, userName, content,
+      submitUpdateTime, onEdit, voteCount, commentCount,
+      voteStatus, onVoted, goComment, wordsCount = 60,
+      title, avatarStyle = 'left', role, signature, requestCommentCount, feedback = false,
+      operation = true
+    } = this.props
+    const { showAll, filterContent, showRequestComment, request } = this.state
+    const renderWorkContent = () => {
+      if(isString(content)) {
+        if(filterContent.length > wordsCount && !showAll) {
           return (
-            <div className={`${avatarStyle}`} onClick={()=>this.show(showAll)}>
-              {truncate(filterContent,{length:wordsCount,omission:''})}
+            <div className={`${avatarStyle}`} onClick={() => this.show(showAll)}>
+              {truncate(filterContent, { length: wordsCount, omission: '' })}
               <span>...</span>
             </div>
           )
         } else {
           return (
-            <pre className={`${avatarStyle}`} dangerouslySetInnerHTML={{__html:content}}/>
+            <pre className={`${avatarStyle}`} dangerouslySetInnerHTML={{ __html: content }}/>
           )
         }
       }
-      return null;
+      return null
     }
 
-    const showOperation = ()=>{
-      if(operation){
-        return true;
+    const showOperation = () => {
+      if(operation) {
+        return true
       } else {
-        return false;
+        return false
       }
     }
 
-    const renderHeader = ()=>{
+    const renderHeader = () => {
       return (
         <div className={`submit-head ${avatarStyle}`}>
           <div className="submit-img"><img className={`submit-avatar ${avatarStyle}`} src={headImage}/></div>
@@ -139,74 +140,81 @@ export default class Work extends React.Component<any,any> {
               <div className="submit-name">
                 {userName}
               </div>
-              {role==3||role==4?<div className="role"><AssetImg url='https://static.iqycamp.com/images/coach.png'/></div>:null}
-              {role==5||role==10?<div className="role"><AssetImg url='https://static.iqycamp.com/images/senior_coach.png'/></div>:null}
-              {role==6||role==8?<div className="role"><AssetImg url='https://static.iqycamp.com/images/first_coach.png'/></div>:null}
-              {role==7?<div className="role"><AssetImg url='https://static.iqycamp.com/images/vip.png'/></div>:null}
+              {role == 3 || role == 4 ?
+                <div className="role"><AssetImg url='https://static.iqycamp.com/images/coach.png'/></div> : null}
+              {role == 5 || role == 10 ?
+                <div className="role"><AssetImg url='https://static.iqycamp.com/images/senior_coach.png'/></div> : null}
+              {role == 6 || role == 8 ?
+                <div className="role"><AssetImg url='https://static.iqycamp.com/images/first_coach.png'/></div> : null}
+              {role == 7 ?
+                <div className="role"><AssetImg url='https://static.iqycamp.com/images/vip.png'/></div> : null}
               <div className="submit-time">{submitUpdateTime}</div>
             </div>
             <div className="signature">{signature}</div>
 
           </div>
 
-          {onEdit?<div className="right" style={{marginTop:`${avatarStyle==='left'?'0':'5px'}`}}>
-              <div className="function-area" onClick={()=>onEdit()} style={{marginRight:8}}>
-                <AssetImg type="edit" height={12}/>
-                <div className="submit-button">
-                  编辑
+          {onEdit ? <div className="right" style={{ marginTop: `${avatarStyle === 'left' ? '0' : '5px'}` }}>
+            <div className="function-area" onClick={() => onEdit()} style={{ marginRight: 8 }}>
+              <AssetImg type="edit" height={12}/>
+              <div className="submit-button">
+                编辑
+              </div>
+            </div>
+            {!request && requestCommentCount != null && requestCommentCount > 0 ?
+              <div className="function-area" onClick={this.click.bind(this)}>
+                <AssetImg type="request_comment" height={12}/>
+                <div className={`submit-button`}>
+                  求点评
                 </div>
               </div>
-              {!request && requestCommentCount!=null && requestCommentCount>0?
-                <div className="function-area" onClick={this.click.bind(this)}>
-                  <AssetImg type="request_comment" height={12}/>
-                  <div className={`submit-button`}>
-                    求点评
-                  </div>
+              : null}
+            {request || (requestCommentCount != null && requestCommentCount === 0) ?
+              <div className="function-area" onClick={this.click.bind(this)}>
+                <AssetImg type="request_comment_disable" height={12}/>
+                <div className={`submit-button disabled`}>
+                  求点评
                 </div>
-                :null}
-              {request || (requestCommentCount!=null && requestCommentCount===0)?<div className="function-area" onClick={this.click.bind(this)}>
-                  <AssetImg type="request_comment_disable" height={12}/>
-                  <div className={`submit-button disabled`}>
-                    求点评
-                  </div>
-                </div>:null}
-            </div>:null}
+              </div> : null}
+          </div> : null}
         </div>
       )
     }
 
     const alertProps = {
-      buttons:[
-        {label:'再想想',onClick:()=>this.setState({showRequestComment:false})},
-        {label:'确定',onClick:()=>this.requestComment()}
-      ],
+      buttons: [
+        { label: '再想想', onClick: () => this.setState({ showRequestComment: false }) },
+        { label: '确定', onClick: () => this.requestComment() }
+      ]
     }
 
     return (
-      <div className={`new-work`} >
-        <Alert { ...alertProps }
-          show={showRequestComment}>
-          <div className="global-pre" dangerouslySetInnerHTML={{__html:`当前课程还剩${requestCommentCount}次请求教练点评的机会<br/>确定要在这次使用吗？`}}/>
+      <div className={`new-work`}>
+        <Alert {...alertProps}
+               show={showRequestComment}>
+          <div className="global-pre"
+               dangerouslySetInnerHTML={{ __html: `当前课程还剩${requestCommentCount}次请求教练点评的机会<br/>确定要在这次使用吗？` }}/>
         </Alert>
         <div className="submit-cell">
           <div className="submit-area">
             {renderHeader()}
-            {title?<div className="submit-title">{title}</div>:null}
-            <div className="submit-content" ref="submitContent" onClick={(e)=>this.contentClick(e)}>{renderWorkContent()}</div>
-            {filterContent && filterContent.length>wordsCount?<div onClick={()=>this.show(showAll)}
-                                                                   className="show-all">{showAll?'收起':'展开'}</div>:null}
+            {title ? <div className="submit-title">{title}</div> : null}
+            <div className="submit-content" ref="submitContent"
+                 onClick={(e) => this.contentClick(e)}>{renderWorkContent()}</div>
+            {filterContent && filterContent.length > wordsCount ? <div onClick={() => this.show(showAll)}
+                                                                       className="show-all">{showAll ? '收起' : '展开'}</div> : null}
             {
               showOperation()
                 ? <div className={`operation-area`}>
-                  <div onClick={()=>{isFunction(goComment)?goComment():null}} className="comment">
+                  <div onClick={() => {isFunction(goComment) ? goComment() : null}} className="comment">
                     <span>{commentCount}</span>
                   </div>
-                  <div onClick={()=>{isFunction(onVoted)?onVoted():null}} className="vote">
-                    <span className={`${voteStatus?'voted':'disVote'}`}>{voteCount}</span>
+                  <div onClick={() => {isFunction(onVoted) ? onVoted() : null}} className="vote">
+                    <span className={`${voteStatus ? 'voted' : 'disVote'}`}>{voteCount}</span>
                   </div>
                   {
                     feedback
-                      ? <div className="operation-asset" onClick={()=>{isFunction(goComment)?goComment():null}}>
+                      ? <div className="operation-asset" onClick={() => {isFunction(goComment) ? goComment() : null}}>
                         <AssetImg
                           url="https://static.iqycamp.com/images/fragment/application_asset_comment.png"
                           width="42px"
