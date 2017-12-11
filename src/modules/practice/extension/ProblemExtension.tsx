1b5abebdfbdc4bcd54193a6321d643a5cf8a2983
xfduan <1047210087@qq.com>
2017-12-11 15:05:08 +0800

    fix style

--- src/modules/practice/extension/ProblemExtension.tsx
+++ src/modules/practice/extension/ProblemExtension.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import { connect } from "react-redux";
-import { startLoad, endLoad, alertMsg } from "../../../redux/actions";
-import AssetImg from "../../../components/AssetImg";
-import { loadProblemExtension } from "../../problem/async";
-import "./ProblemExtension.less";
+import * as React from 'react'
+import { connect } from 'react-redux'
+import { startLoad, endLoad, alertMsg } from '../../../redux/actions'
+import AssetImg from '../../../components/AssetImg'
+import { loadProblemExtension } from '../../problem/async'
+import './ProblemExtension.less'
 
 @connect(state => state)
 export default class ProblemExtension extends React.Component<any, any> {
@@ -28,7 +28,7 @@ export default class ProblemExtension extends React.Component<any, any> {
       if(res.code === 200) {
         this.setState({ data: res.msg, extension: res.msg.extension })
       } else {
-        dispatch(alertMsg("当前课程暂无延伸学习相关内容"))
+        dispatch(alertMsg('当前课程暂无延伸学习相关内容'))
       }
     }).catch(e => {
       dispatch(endLoad())
@@ -46,7 +46,7 @@ export default class ProblemExtension extends React.Component<any, any> {
         return (
           <div className="extension-activity">
             <ExtensionHead
-              icon={{ uri: "https://static.iqycamp.com/images/problem/extension_icon_bag.png", width: 21, height: 20 }}
+              icon={{ uri: 'https://static.iqycamp.com/images/problem/extension_icon_bag.png', width: 21, height: 20 }}
               content={`学习活动`}/>
             {renderOnlineActivities()}
             {renderOfflineActivities()}
@@ -59,7 +59,6 @@ export default class ProblemExtension extends React.Component<any, any> {
       if(onlineActivities && onlineActivities.length > 0) {
         return (
           <div style={{ marginBottom: 20 }}>
-            <ActivityTypeTitle content={`线上活动`}/>
             {
               onlineActivities.map((item, index) =>
                 <OnlineContentBox description={item.description} password={item.password} uri={item.uri} key={index}/>
@@ -74,7 +73,6 @@ export default class ProblemExtension extends React.Component<any, any> {
       if(offlineActivities && offlineActivities.length > 0) {
         return (
           <div>
-            <ActivityTypeTitle content={`线下活动`}/>
             {
               offlineActivities.map((item, index) =>
                 <OfflineContentBox description={item.description} location={item.location} workshop={item.workshop}
@@ -87,27 +85,25 @@ export default class ProblemExtension extends React.Component<any, any> {
     }
 
     return (
-      <div className="problem-extension-container" style={{ minHeight: window.innerHeight }}>
-        <div className="problem-extension-page">
-          <div className="extension-head"/>
-          <div className="extension-read">
-            <ExtensionHead
-              icon={{ uri: "https://static.iqycamp.com/images/problem/extension_icon_book.png", width: 21, height: 20 }}
-              content={`延伸阅读`}/>
-            <div className="extension-read-content" dangerouslySetInnerHTML={{ __html: extension }}/>
-          </div>
-          {renderActivities()}
-          <div className="extension-share">
-            <ExtensionHead
-              icon={{ uri: "https://static.iqycamp.com/images/problem/extension_icon_star.png", width: 27, height: 20 }}
-              content={`课程分享`}/>
-            <div className="extension-share-content">深度好文&nbsp;遇见大咖&nbsp;分享心得</div>
-            <div className="extension-share-view"
-                 onClick={() => this.context.router.push({
-                   pathname: "/rise/static/practice/subject",
-                   query: { id: problemId, series }
-                 })}>进入
-            </div>
+      <div className="problem-extension-container">
+        {/*<div className="extension-head"/>*/}
+        <div className="extension-read">
+          <ExtensionHead
+            icon={{ uri: 'https://static.iqycamp.com/images/problem/extension_icon_book.png', width: 21, height: 20 }}
+            content={`延伸阅读`}/>
+          <div className="extension-read-content" dangerouslySetInnerHTML={{ __html: extension }}/>
+        </div>
+        {renderActivities()}
+        <div className="extension-share">
+          <ExtensionHead
+            icon={{ uri: 'https://static.iqycamp.com/images/problem/extension_icon_star.png', width: 27, height: 20 }}
+            content={`课程分享`}/>
+          <div className="extension-share-content">深度好文&nbsp;遇见大咖&nbsp;分享心得</div>
+          <div className="extension-share-view"
+               onClick={() => this.context.router.push({
+                 pathname: '/rise/static/practice/subject',
+                 query: { id: problemId, series }
+               })}>进入
           </div>
         </div>
       </div>
@@ -117,20 +113,18 @@ export default class ProblemExtension extends React.Component<any, any> {
 }
 
 // 分类上 icon 和文字
-class ExtensionHead extends React.Component<{ icon: { uri: string; width: number; height: number }; content: string }, any> {
+class ExtensionHead extends React.Component<{icon: {uri: string; width: number; height: number}; content: string}, any> {
   render() {
     const { icon, content } = this.props
     const { uri, width, height } = icon
     return (
-      <div className="extension-head-title">
-        {/*<div className="extension-head-icon"><AssetImg url={uri} width={width} height={height}/></div>*/}
-        <div className="extension-head-content">{content}</div>
-      </div>
+      <div className="extension-head-title">{content}</div>
     )
   }
 }
+
 // 活动分类
-class ActivityTypeTitle extends React.Component<{ content: string }, any> {
+class ActivityTypeTitle extends React.Component<{content: string}, any> {
   render() {
     const { content } = this.props
     return (
@@ -138,8 +132,9 @@ class ActivityTypeTitle extends React.Component<{ content: string }, any> {
     )
   }
 }
+
 // 线上活动列表
-class OnlineContentBox extends React.Component<{ description: string, password: string, uri: string }, any> {
+class OnlineContentBox extends React.Component<{description: string, password: string, uri: string}, any> {
   render() {
     const { description, password, uri } = this.props
     return (
@@ -148,13 +143,14 @@ class OnlineContentBox extends React.Component<{ description: string, password: 
         {
           password ? <div className="online-password">直播间密码:{password}</div> : null
         }
-        <div className="online-view" onClick={() => window.location.href = uri}>进入</div>
+        <div className="online-view" onClick={() => window.location.href = uri}>点击查看</div>
       </div>
     )
   }
 }
+
 // 线下活动列表
-class OfflineContentBox extends React.Component<{ description: string, location: string, workshop: string, uri: string }, any> {
+class OfflineContentBox extends React.Component<{description: string, location: string, workshop: string, uri: string}, any> {
   render() {
     const { description, location, workshop, uri } = this.props
     return (
