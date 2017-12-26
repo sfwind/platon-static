cd2a3bdafbd7413198a5581cd93f97c6d5871b88
richardyangcz <richardyangcz@sina.com>
2017-12-26 16:52:54 +0600

    add prize_card

---
+++ src/modules/annual/async.ts
@@ -0,0 +1,17 @@
+import { pget } from '../../utils/request'
+
+export function loadAnnualPrizeCards(riseId) {
+  return pget(`/rise/operation/prize/load/annual/${riseId}`)
+}
+
+export function loadPersonalAnnualPrizeCard() {
+  return pget(`/rise/operation/prize/load/annual`)
+}
+
+export function receiveAnnualCards(cardId) {
+  return pget(`/rise/operation/prize/annual/receive/${cardId}`)
+}
+
+export function checkSubscribe(callback, key) {
+  return pget(`/rise/customer/check/subscribe/${key}`, { callback: callback })
+}
