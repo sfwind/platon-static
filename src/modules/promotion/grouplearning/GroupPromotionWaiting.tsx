e646680662cf9cb2acdfa46b8c42450c8b0da124
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-12-27 14:15:48 +0800

    update content

--- src/modules/promotion/grouplearning/GroupPromotionWaiting.tsx
+++ src/modules/promotion/grouplearning/GroupPromotionWaiting.tsx
@@ -24,8 +24,8 @@ export class GroupPromotionWaiting extends React.Component<GroupPromotionWaiting
     return (
       <section className="group-promotion-waiting">
         <div className="waiting-image"/>
-        <span className={'waiting-tip-large'}>你已接受{leaderName}邀请<br/>加入自我认识实验室！</span>
-        <span className={'waiting-tip-small'}>还差最后{remainderCount}人加入，解锁前7天实验：<br/>请等待解锁成功通知</span>
+        <span className={'waiting-tip-large'}>你已接受{leaderName}邀请<br/>加入自我认识试听！</span>
+        <span className={'waiting-tip-small'}>还差最后{remainderCount}人加入，解锁前7天课程：<br/>请等待解锁成功通知</span>
         <span className={'waiting-invite'}
               onClick={() => {
                 window.location.href = `https://${window.location.hostname}/pay/static/camp/group?groupCode=${groupCode}&share=true`
