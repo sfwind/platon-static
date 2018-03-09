adc531a8194ba165439724a4da73a456dcd5d480
xfduan <1047210087@qq.com>
2018-03-09 11:15:34 +0800

    remove

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.join(__dirname, '__build__'),
     filename: '[name].bundle.js',
     chunkFilename: '[id].chunk.js',
-    publicPath: '/'
+    publicPath: '/rise_js/'
   },
   plugins: [],
   resolve: {
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
