dc44331f41879f6b9a8f7474eee187c0d8386cf5
Ding Zhi Jun <dingzhijun@imdada.cn>
2018-03-19 17:51:36 +0800

    change url

--- src/modules/base/ToolBar.tsx
+++ src/modules/base/ToolBar.tsx
@@ -88,13 +88,16 @@ export class ToolBar extends React.Component<any, any> {
     // check url
     const { dispatch } = this.props
     let tabIndex = 0
-    if (window.location.pathname === '/rise/static/rise' || window.location.pathname === '/rise/static/camp' || window.location.pathname === '/rise/static/learn') {
+    if (window.location.pathname === '/rise/static/rise' ||
+      window.location.pathname === '/rise/static/camp' ||
+      window.location.pathname === '/rise/static/learn') {
       tabIndex = 0
     } else if (window.location.pathname === '/rise/static/event/wall') {
       tabIndex = 1
     } else if (window.location.pathname === '/rise/static/problem/explore') {
       tabIndex = 2
-    } else if (window.location.pathname.indexOf('/rise/static/customer') != -1 || window.location.pathname.indexOf('/rise/static/message') != -1) {
+    } else if (window.location.pathname.indexOf('/rise/static/customer') != -1 ||
+      window.location.pathname.indexOf('/rise/static/message') != -1) {
       //消息中心和个人中心
       tabIndex = 3
     }
@@ -115,11 +118,6 @@ export class ToolBar extends React.Component<any, any> {
     const { dispatch } = this.props
     dispatch(set('tabIndex', tabIndex))
     if (tabIndex === 0) {
-      // if (window.ENV.showExplore !== 'false') {
-      //   this.context.router.push('/rise/static/rise')
-      // } else {
-      //   this.context.router.push('/rise/static/course/schedule/plan')
-      // }
       this.context.router.push('/rise/static/learn')
     } else if (tabIndex === 1) {
       this.context.router.push('/rise/static/event/wall')
