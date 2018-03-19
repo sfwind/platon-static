45b5fb06cecb104f1d249d6b13a875f4ba370dbc
richardyangcz <richardyangcz@sina.com>
2018-03-19 17:35:19 +0600

    add empty study list

--- src/modules/customer/person/StudyList.tsx
+++ src/modules/customer/person/StudyList.tsx
@@ -6,6 +6,7 @@ import { loadStudyReport } from '../async'
 import { connect } from 'react-redux'
 import { set, startLoad, endLoad, alertMsg } from 'reduxutil/actions'
 import { MarkBlock } from '../../../components/markblock/MarkBlock'
+import * as _ from 'lodash'
 
 @connect(state => state)
 export default class StudyList extends React.Component {
@@ -42,8 +43,18 @@ export default class StudyList extends React.Component {
     const { data = [] } = this.state
     return (
       <div className="study-list-container">
-        {
-          data.map((plan, index) => {
+        {_.isEmpty(data)?
+          <div className="no-study-container">
+            <img src="https://static.iqycamp.com/images/no-certificate.png" style={{ width: 100, height: 100 }}/>
+            <div className="show-no-study">
+              暂时没有形成的学习报告哦
+            </div>
+            <div className="show-info">
+              速速去上课获取吧
+            </div>
+          </div>
+          :
+            data.map((plan, index) => {
             const { problem, point, name, planId, problemId } = plan
             return (
               <MarkBlock module={'打点'} function={'我的学习报告'} action={'查看我的学习报告'} memo={planId} className="card-section"
