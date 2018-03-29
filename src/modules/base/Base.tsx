92c4abe9f0377e63627db02793dfd684485b19b0
nethunder <17089113140@163.com>
2018-03-29 15:49:45 +0800

    update

--- src/modules/base/Base.tsx
+++ src/modules/base/Base.tsx
@@ -72,12 +72,11 @@ export default class Main extends React.Component<any, any> {
       window.ENV.headImgUrl = userInfoResult.msg.headimgurl;
     }
 
+    console.log(`${window.ENV.sensorsProject}`)
     sa.init({
-      sdk_url: 'https://static.sensorsdata.cn/sdk/1.9.13/sensorsdata.min.js',
-      heatmap_url: 'https://static.sensorsdata.cn/sdk/1.9.13/heatmap.min.js',
       name: 'sa',
-      web_url: 'https://quanwai.cloud.sensorsdata.cn/',
-      server_url: 'https://quanwai.cloud.sensorsdata.cn:4006/sa?token=0a145b5e1c9814f4',
+      web_url: `https://quanwai.cloud.sensorsdata.cn/?project=${window.ENV.sensorsProject}`,
+      server_url: `https://quanwai.cloud.sensorsdata.cn:4006/sa?token=0a145b5e1c9814f4&project=${window.ENV.sensorsProject}`,
       heatmap: {},
       is_single_page: true,
     });
