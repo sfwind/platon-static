6c5de2ef8b1ead5852d486a7ca8598c78b678085
三十文 <1047210087@qq.com>
2018-04-19 18:29:47 +0800

    fix submit style

--- src/modules/practice/common/ApplicationSubmit/ApplicationSubmit.tsx
+++ src/modules/practice/common/ApplicationSubmit/ApplicationSubmit.tsx
@@ -1,94 +1,94 @@
-import * as React from 'react'
-import './ApplicationSubmit.less'
-import Editor from '../../../../components/simditor/Editor'
-import requestProxy from '../../../../components/requestproxy/requestProxy'
-import { autoSaveApplicationDraft, loadApplicationPractice, submitApplicationPractice } from '../../application/async'
+import * as React from 'react';
+import './ApplicationSubmit.less';
+import Editor from '../../../../components/simditor/Editor';
+import requestProxy from '../../../../components/requestproxy/requestProxy';
+import { autoSaveApplicationDraft, loadApplicationPractice, submitApplicationPractice } from '../../application/async';
 
-const APPLICATION_AUTO_SAVING = 'rise_application_autosaving'
+const APPLICATION_AUTO_SAVING = 'rise_application_autosaving';
 
 export default class ApplicationSubmit extends React.Component {
 
-  autoSaveTimer = null
+  autoSaveTimer = null;
 
   constructor () {
-    super()
+    super();
     this.state = {
       value: '',
-    }
+    };
   }
 
   static contextTypes = {
     router: React.PropTypes.object.isRequired,
-  }
+  };
 
   componentWillMount () {
-    // document.body.addEventListener('mousewheel', this.lockWindow)
-    // document.body.addEventListener('touchmove', this.lockWindow)
+    document.body.addEventListener('mousewheel', this.lockWindow)
+    document.body.addEventListener('touchmove', this.lockWindow)
   }
 
   lockWindow (e) {
-    e.preventDefault()
+    e.preventDefault();
   }
 
   async componentDidMount () {
-    const { id, planId } = this.props
-    let res = await loadApplicationPractice(id, planId)
-    let storageDraft = JSON.parse(window.localStorage.getItem(APPLICATION_AUTO_SAVING))
+    const { id, planId } = this.props;
+    let res = await loadApplicationPractice(id, planId);
+    let storageDraft = JSON.parse(window.localStorage.getItem(APPLICATION_AUTO_SAVING));
     if (storageDraft && storageDraft.id === id) {
       if (res.msg.overrideLocalStorage || res.msg.isSynchronized) {
         this.setState({
           value: res.msg.isSynchronized ? res.msg.content : res.msg.draft,
         }, () => {
-          this.autoSaveApplicationDraft()
-        })
+          this.autoSaveApplicationDraft();
+        });
       } else {
         this.setState({
           value: storageDraft.content,
         }, () => {
-          this.autoSaveApplicationDraft()
-        })
+          this.autoSaveApplicationDraft();
+        });
       }
     } else {
       this.setState({
         value: res.msg.isSynchronized ? res.msg.content : res.msg.draft,
       }, () => {
-        this.autoSaveApplicationDraft()
-      })
+        this.autoSaveApplicationDraft();
+      });
     }
   }
 
   componentWillUnmount () {
-    clearInterval(this.autoSaveTimer)
-    // document.body.removeEventListener('mousewheel', this.lockWindow)
-    // document.body.removeEventListener('touchmove', this.lockWindow)
+    clearInterval(this.autoSaveTimer);
+    document.body.removeEventListener('mousewheel', this.lockWindow)
+    document.body.removeEventListener('touchmove', this.lockWindow)
   }
 
   autoSaveApplicationDraft () {
-    clearInterval(this.autoSaveTimer)
-    const { id, planId } = this.props
+    clearInterval(this.autoSaveTimer);
+    const { id, planId } = this.props;
     this.autoSaveTimer = setInterval(() => {
       if (this.refs.editor) {
-        let draft = this.refs.editor.getValue()
+        let draft = this.refs.editor.getValue();
         if (draft.trim().length > 0) {
-          autoSaveApplicationDraft(planId, id, draft)
+          autoSaveApplicationDraft(planId, id, draft);
         }
       }
-    }, 10000)
+    }, 10000);
   }
 
   autoSave () {
     if (this.refs.editor) {
-      let value = this.refs.editor.getValue()
+      let value = this.refs.editor.getValue();
       if (value) {
         window.localStorage.setItem(APPLICATION_AUTO_SAVING, JSON.stringify({
           id: this.props, content: value,
-        }))
+        }));
       }
     }
   }
 
   clearStorage () {
-    window.localStorage.removeItem(APPLICATION_AUTO_SAVING)
+    window.localStorage.removeItem(APPLICATION_AUTO_SAVING);
   }
 
   async handleSubmitApplicationSubmit () {
@@ -97,18 +97,18 @@ export default class ApplicationSubmit extends React.Component {
       },
       submitCallback = () => {
       },
-    } = this.props
-    const value = this.refs.editor.getValue()
+    } = this.props;
+    const value = this.refs.editor.getValue();
     if (!value || value.length === 0) {
-      requestProxy.alertMessage('请填写作业')
-      return
+      requestProxy.alertMessage('请填写作业');
+      return;
     } else {
-      const { id, planId } = this.props
-      let res = await submitApplicationPractice(planId, id, { answer: value })
+      const { id, planId } = this.props;
+      let res = await submitApplicationPractice(planId, id, { answer: value });
       if (res.code === 200) {
-        this.clearStorage()
-        hideCallback()
-        submitCallback()
+        this.clearStorage();
+        hideCallback();
+        submitCallback();
       }
     }
   }
@@ -121,8 +121,8 @@ export default class ApplicationSubmit extends React.Component {
       },
       submitCallback = () => {
       },
-    } = this.props
-    const { value } = this.state
+    } = this.props;
+    const { value } = this.state;
 
     return (
       <div className="application-submit-component">
@@ -141,8 +141,8 @@ export default class ApplicationSubmit extends React.Component {
           <div className="footerbutton">
             <div className="temp-save"
                  onClick={() => {
-                   hideCallback()
-                   autoSaveApplicationDraft(planId, id, this.refs.editor.getValue())
+                   hideCallback();
+                   autoSaveApplicationDraft(planId, id, this.refs.editor.getValue());
                  }}>保存并返回
             </div>
             <div className="submit"
@@ -150,9 +150,13 @@ export default class ApplicationSubmit extends React.Component {
             </div>
           </div>
         </div>
-        <div className="mask"></div>
+        <div className="mask"
+             onClick={() => {
+               hideCallback();
+               autoSaveApplicationDraft(planId, id, this.refs.editor.getValue());
+             }}></div>
       </div>
-    )
+    );
   }
 
 }
