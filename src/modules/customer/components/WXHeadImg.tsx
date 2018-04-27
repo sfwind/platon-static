e0d64865843b3bf2e65480624eb3304c070034d1
richardyangcz <richardyangcz@sina.com>
2018-04-27 14:39:29 +0600

    fix

--- src/modules/customer/components/WXHeadImg.tsx
+++ src/modules/customer/components/WXHeadImg.tsx
@@ -18,26 +18,26 @@ export default class WXHeadImg extends React.Component<inputProps, any> {
 
   componentDidMount() {
     setTimeout( ()=>{
-      let expired_hash_value = this.props.expired_hash_value
-      let img = this.refs.img
+      let expired_hash_value = this.props.expired_hash_value;
+      let img = this.refs.img;
 
-      img.setAttribute('crossOrigin', 'Anonymous')
+      img.setAttribute('crossOrigin', 'Anonymous');
       img.onload = () => {
-        let canvas = document.createElement('canvas')
-        canvas.width = 100
-        canvas.height = 100
+        let canvas = document.createElement('canvas');
+        canvas.width = 100;
+        canvas.height = 100;
 
-        let ctx = canvas.getContext('2d')
-        ctx.drawImage(img, 0, 0)
+        let ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
 
-        let dataUrl = canvas.toDataURL('image/png')
-        let hash_value = md5(dataUrl)
+        let dataUrl = canvas.toDataURL('image/png');
+        let hash_value = md5(dataUrl);
         //判断两个对象的内容是否相等
         if(_.isEqual(expired_hash_value, hash_value)) {
-          updateWXImg(this.props.riseId)
+          updateWXImg(this.props.riseId);
         }
       }
-      img.src = this.props.src.replace('https', 'http')
+      img.src = this.props.src.replace('https', 'http');
     },300);
   }
 
